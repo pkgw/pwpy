677762ff956f76460fa9d411debe051dc45688ba
peter <peter@69fc08fc-fdfc-0310-ace8-9e8da0474e22>
2007-03-01 23:01:59 +0000

    Updates to miriad.py
    git-svn-id: https://newton.cx/svn/trunk/pylib@810 69fc08fc-fdfc-0310-ace8-9e8da0474e22

--- miriad.py
+++ miriad.py
@@ -1,4 +1,7 @@
 # Wrapper for invoking MIRIAD scripts
+#
+# 'mirhelp uvflag' has good help on the 'select' and
+# 'line' parameters.
 
 import sys, os, re, math
 from os.path import join
@@ -63,7 +66,8 @@ class TaskBase (object):
     
     _name = None
     _params = None
-
+    _options = None
+    
     def __init__ (self, **kwargs):
         self.setArgs (**kwargs)
 
@@ -73,15 +77,40 @@ class TaskBase (object):
             
     def launch (self, **kwargs):
         cmd = [join (_bindir, self._name)]
-        params = self._params or []
+        options = []
+
+        # Options
         
-        for name in params:
+        for opt in self._options or []:
+            val = getattr (self, opt)
+
+            if val is None: continue
+            
+            if isinstance (val, bool):
+                if val: options.append (opt)
+            else:
+                options.append (opt)
+
+                if not hasattr (val, '__iter__'):
+                    options.append (str (val))
+                else:
+                    for x in val:
+                        options.append (str (x))
+
+        if len (options) > 0:
+            cmd.append ('options=%s' % (','.join (options)))
+
+        # Parameters
+        
+        for name in self._params or []:
             if name[-1] == '_': key = name[:-1]
             else: key = name
             
             val = getattr (self, name)
             if val: cmd.append ("%s=%s" % (key, val))
 
+        # Now go.
+        
         self.cmdline = ' '.join (cmd)
         self.proc = Popen (cmd, stdout=PIPE, stderr=PIPE, shell=False, **kwargs)
 
@@ -142,13 +171,31 @@ class TaskCgDisp (TaskBase):
     boxfac = None
 
 class TaskUVList (TaskBase):
-    # XXX FIXME: incomplete set of keywords
     _name = 'uvlist'
-    _params = ['recnum', 'vis', 'options']
-
-    recnum = 1000
+    _params = ['vis', 'select', 'line', 'scale', 'recnum', 'log']
+    _options = ['brief', 'data', 'average', 'allan', 'history',
+                'flux', 'full', 'list', 'variables', 'stat',
+                'birds', 'spectra']
+    
     vis = None
-    options = 'variables'
+    select = None
+    line = None
+    scale = None
+    recnum = 1000
+    log = None
+
+    brief = False
+    data = False
+    average = False
+    allan = False
+    history = False
+    flux = False
+    full = False
+    list = False
+    variables = True
+    stat = False
+    birds = False
+    spectra = False
 
 class TaskUVPlot (TaskBase):
     # XXX FIXME: incomplete set of keywords
@@ -164,19 +211,33 @@ class TaskUVPlot (TaskBase):
 class TaskInvert (TaskBase):
     # XXX FIXME: incomplete set of keywords
     _name = 'invert'
-    _params = ['vis', 'map', 'beam', 'select', 'stokes', 'options',
+    _params = ['vis', 'map', 'beam', 'select', 'stokes',
                'robust', 'cell', 'fwhm', 'imsize']
-
+    _options = ['nocal', 'nopol', 'nopass', 'double', 'systemp',
+                'mfs', 'sdb', 'mosaic', 'imaginary', 'amplitude',
+                'phase']
+    
     vis = None
     map = None
     beam = None
     select = None
     stokes = 'ii'
-    options = 'mfs,systemp,double'
     robust = None
     cell = None
     fwhm = None
     imsize = None
+
+    nocal = False
+    nopol = False
+    nopass = False
+    double = True
+    systemp = True
+    mfs = True
+    sdb = False
+    mosaic = False
+    imaginary = False
+    amplitude = False
+    phase = False
 
 class TaskClean (TaskBase):
     # XXX FIXME: incomplete set of keywords
@@ -201,18 +262,35 @@ class TaskRestore (TaskBase):
 
 class TaskImStat (TaskBase):
     _name = 'imstat'
-    _params = ['in_', 'region', 'options', 'plot', 'cutoff',
+    _params = ['in_', 'region', 'plot', 'cutoff',
                'beam', 'axes', 'device', 'log']
-
+    _options = ['tb', 'hanning', 'boxcar', 'deriv', 'noheader',
+                'nolist', 'eformat', 'guaranteespaces', 'xmin',
+                'xmax', 'ymin', 'ymax', 'title', 'style']
+               
     in_ = None
     beam = None
     region = None
-    options = None
     plot = None
     cutoff = None
     axes = None
     device = None
     log = None
+
+    tb = False
+    hanning = None
+    boxcar = None
+    deriv = None
+    noheader = False
+    nolist = False
+    eformat = False
+    guaranteespaces = False
+    xmin = None
+    xmax = None
+    ymin = None
+    ymax = None
+    title = None
+    style = None
     
 class TaskImHead (TaskBase):
     _name = 'imhead'
@@ -234,15 +312,106 @@ class TaskImHead (TaskBase):
 
 class TaskIMom (TaskBase):
     _name = 'imom'
-    _params = ['in_', 'options', 'region', 'min', 'max', 'log']
-
+    _params = ['in_', 'region', 'min', 'max', 'log']
+    _options = ['skew', 'clipmean', 'clip1sigma']
+    
     in_ = None
     options = None
     region = None
     min = None
     max = None
     log = None
-        
+
+    skew = False
+    clipmean = False
+    clip1sigma = False
+    
+class TaskImFit (TaskBase):
+    _name = 'imfit'
+    _params = ['in_', 'region', 'clip', 'object', 'spar',
+               'fix', 'out']
+    _options = ['residual']
+    
+    in_ = None
+    region = None
+    clip = None
+    object = None
+    spar = None
+    fix = None
+    out = None
+    options = None
+
+    residual = False
+    
+class TaskUVAver (TaskBase):
+    _name = 'uvaver'
+    _params = ['vis', 'select', 'line', 'ref', 'stokes',
+               'interval', 'out']
+    _options = ['nocal', 'nopass', 'nopol', 'relax',
+                'vector', 'scalar', 'scavec']
+    
+    vis = None
+    select = None
+    line = None
+    ref = None
+    stokes = None
+    interval = None
+    options = None
+    out = None
+
+    nocal = False
+    nopass = False
+    nopol = False
+    relax = False
+    vector = False
+    scalar = False
+    scavec = False
+
+class TaskGPCopy (TaskBase):
+    _name = 'gpcopy'
+    _params = ['vis', 'out', 'mode']
+    _options = ['nopol', 'nocal', 'nopass']
+
+    vis = None
+    out = None
+    mode = None
+
+    nopol = False
+    nocal = False
+    nopass = False
+
+class TaskMSelfCal (TaskBase):
+    _name = 'mselfcal'
+    _params = ['vis', 'select', 'model', 'clip', 'interval',
+               'minants', 'refant', 'flux', 'offset', 'line',
+               'out']
+    _options = ['amplitude', 'phase', 'smooth', 'polarized',
+                'mfs', 'relax', 'apriori', 'noscale', 'mosaic',
+                'verbose']
+
+    vis = None
+    select = None
+    model = None
+    clip = None
+    interval = None
+    minants = None
+    refant = None
+    flux = None
+    offset = None
+    line = None
+    out = None
+
+    amplitude = False
+    phase = False
+    smooth = False
+    polarized = False
+    mfs = False
+    relax = False
+    apriori = False
+    noscale = False
+    mosaic = False
+    verbose = False
+
 # These functions operate on single images, using several of
 # the tasks defined above.
 
@@ -260,7 +429,7 @@ def getImageDimensions (image, **kwargs):
 def getImageStats (image, **kwargs):
     # FIXME: noheader option seems a little dangerous, if we
     # ever use this for multifreq data.
-    ims = TaskImStat (in_=image, options='noheader', **kwargs)
+    ims = TaskImStat (in_=image, noheader=True, **kwargs)
     (stdout, stderr) = ims.snarf ()
         
     if len(stdout) != 2:
@@ -313,3 +482,65 @@ def getImageBeamSize (image, **kwargs):
     bmin = float (imh.snarfOne ('bmin')) # in radians
     
     return (bmaj, bmin)
+
+def fitImagePoint (image, **kwargs):
+    imf = TaskImFit (in_=image, **kwargs)
+    imf.object = 'point'
+    
+    (stdout, stderr) = imf.snarf ()
+
+    rms = max = None
+    
+    for line in stdout:
+        if 'RMS residual' in line:
+            a = line.split (' ')
+            rms = float (a[3])
+        elif 'Peak value:' in line:
+            # '  Peak value:                 6.9948E-04 +/-  0.0000'
+            #  012345678901234567890123456789012345678901234567890123456
+            #  0         1         2         3         4         5      
+            max = float (line[30:40])
+
+    if not rms or not max:
+        raise Exception ('Didn\'t get all info from imfit routine!')
+
+    return (max, rms)
+
+# Simple object representing a MIRIAD data set of some kind or another.
+# gb.py has VisData and ImageData subclasses, etc.
+
+class MiriadData (object):
+    def __init__ (self, basedata):
+        self.base = basedata
+
+    def __str__ (self):
+        return self.base
+
+    def __repr__ (self):
+        return '<MIRIAD data, base "%s">' % self.base
+
+    @property
+    def exists (self):
+        """True if the data specified by this class actually exists.
+        (If False, the data corresponding to this object will probably
+        be created by the execution of a command.)"""
+        return os.path.exists (self.base)
+
+    def checkExists (self):
+        if self.exists: return
+
+        raise Exception ('Data set %s does not exist' % self.base)
+    
+    def delete (self):
+        # Silently not doing anything seems appropriate here.
+        if not self.exists: return
+        
+        for e in os.listdir (self.base):
+            os.remove (join (self.base, e))
+        os.rmdir (self.base)
+
+    def makeVariant (self, kind, name):
+        if not issubclass (kind, MiriadData): raise Exception ('blarg')
+
+        return kind (self.base + '.' + name)
+    
