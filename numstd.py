c6f580ea1da3319b3e254391dcd41ab44edf405c
peter <peter@69fc08fc-fdfc-0310-ace8-9e8da0474e22>
2007-04-12 01:19:05 +0000

    Little utility
    git-svn-id: https://newton.cx/svn/trunk/pylib@816 69fc08fc-fdfc-0310-ace8-9e8da0474e22

---
+++ numstd.py
@@ -0,0 +1,8 @@
+# A few useful functions from numpy
+
+from numpy import linspace, logspace, ones, zeros, identity
+from numpy import asarray, array, hstack, vstack
+from numpy import where, any, all
+from numpy import sum, dot, min, max
+from numpy import sin, cos, tan, sinh, cosh, tanh
+from numpy import arcsin, arccos, arctan, arcsinh, arccosh, arctanh
