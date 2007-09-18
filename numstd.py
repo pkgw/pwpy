47412ea117fb11ab951550976af81fc2dedcf0a6
peter <peter@69fc08fc-fdfc-0310-ace8-9e8da0474e22>
2007-09-18 20:03:07 +0000

    Also import median in numstd
    git-svn-id: https://newton.cx/svn/trunk/pylib@921 69fc08fc-fdfc-0310-ace8-9e8da0474e22

--- numstd.py
+++ numstd.py
@@ -5,7 +5,7 @@ import numpy
 from numpy import linspace, logspace, ones, zeros, identity
 from numpy import asarray, array, ndarray, hstack, vstack
 from numpy import where, any, all
-from numpy import sum, dot, min, max
+from numpy import sum, dot, min, max, median
 from numpy import sin, cos, tan, sinh, cosh, tanh
 from numpy import arcsin, arccos, arctan, arcsinh, arccosh, arctanh
 from numpy import log, log10, exp, sqrt
