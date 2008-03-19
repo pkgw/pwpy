8e097a6edd449faeba997b058a3ea3a5fadc089d
peter <peter@69fc08fc-fdfc-0310-ace8-9e8da0474e22>
2008-03-19 22:29:58 +0000

    Start working on a Fit class that does things right.
    git-svn-id: https://newton.cx/svn/trunk/pylib@1078 69fc08fc-fdfc-0310-ace8-9e8da0474e22

--- fit.py
+++ fit.py
@@ -125,7 +125,7 @@ def gaussian (x, y, params=None):
 
     return pfit
 
-def generic (model, x, y, params, **kwargs):
+def generic (model, x, y, params, weights=None, **kwargs):
     """Generic least-squares fitting algorithm. Parameters are:
 
     model    - A function of N+1 variables: an ndarray of X values,
@@ -134,6 +134,9 @@ def generic (model, x, y, params, **kwargs):
     y        - The data Y values.
     params   - A tuple of N values that are the initial guesses for
                the parameters to model().
+    weights  - Optional array of weights to assign to the data points.
+               Typically, weights = 1 / uncertainties. The sum of the
+               weights need not be 1 (or any other particular value).
     **kwargs - Optional extra parameters to pass to the fitting
                function, scipy.optimize.leastsq().
 
@@ -146,9 +149,15 @@ def generic (model, x, y, params, **kwargs):
 
     x = asarray (x)
     y = asarray (y)
-    
-    def error (p):
-        return ravel (model (x, *p) - y)
+
+    if weights is None:
+        def error (p):
+            return ravel (model (x, *p) - y)
+    else:
+        weights = asarray (weights)
+        
+        def error (p):
+            return ravel ((model (x, *p) - y) * weights)
 
     pfit, xx, xx, msg, success = optimize.leastsq (error, params,
                                                    full_output=True, **kwargs)
@@ -161,14 +170,14 @@ def generic (model, x, y, params, **kwargs):
 def _gausslinModel (x, y0, m, height, xmid, width):
     return (y0 + x * m) + height * _exp (-0.5 * ((x - xmid)/width)**2)
 
-def gausslin (x, y, params=None):
+def gausslin (x, y, params=None, weights=None):
     """Least-squares fit of a Gaussian plus a linear offset. The parameter
     tuple is of the form (y0, m, height, xmid, width)."""
     
     if params is None:
         params = (0, 0) + guessGaussianParams (x, y)
 
-    return generic (_gausslinModel, x, y, params)
+    return generic (_gausslinModel, x, y, params, weights)
 
 # This is all copied from the Scipy Cookbook page on "Fitting Data"
 
@@ -216,7 +225,7 @@ def gauss2d (data, guess=None, getResid=False, **kwargs):
     model = makeGauss2dFunc (*pfit)(*_numpy.indices (data.shape))
     return pfit, data - model
 
-def power (x, y, params=None, **kwargs):
+def power (x, y, params=None, weights=None, **kwargs):
     """Least-squares fit of a power law: y = q * x**alpha.
 
     Returns: q, alpha.
@@ -225,7 +234,7 @@ def power (x, y, params=None, **kwargs):
     l = _numpy.log
     
     def model (x, q, alpha):
-        return q * x **alpha
+        return q * x ** alpha
 
     if params is None:
         dlogx = l (x.max ()) - l (x.min ())
@@ -238,4 +247,249 @@ def power (x, y, params=None, **kwargs):
 
         params = (q, alpha)
 
-    return generic (model, x, y, params, **kwargs)
+    return generic (model, x, y, params, weights, **kwargs)
+
+import numpy as _N
+
+class FitBase (object):
+    _paramNames = None
+
+    def __init__ (self):
+        if self._paramNames is None:
+            raise Exception ('FitBase implementation %s needs to set _paramNames' \
+                             % (self.__class__))
+    
+    def setData (self, x, y, sigmas=None):
+        self.x = _N.asarray (x)
+        self.y = _N.asarray (y)
+
+        if sigmas is None:
+            self.sigmas = None
+        else:
+            self.sigmas = _N.asarray (sigmas)
+
+        return self
+
+    def fakeSigmas (self, val):
+        self.sigmas = _N.zeros_like (self.x) + val
+        return self
+    
+    def guess (self):
+        """Return a tuple of parameter guesses based on the X and Y data."""
+        raise NotImplementedError ()
+
+    def makeModel (self, *params):
+        """Return a function that maps X values into appropriate
+        model values."""
+        
+        raise NotImplementedError ()
+
+    def _fitImpl (self, x, y, sig, guess):
+        """Obtain a fit in some way, and set at least the following
+        fields:
+        
+          params - a tuple of best-fit parameters (compatible with the result of guess)
+          uncerts - A tuple of uncertainties of the parameters.
+
+        Can also set the following fields if their values are derived during
+        the fit:
+
+          mfunc - A function that evaluates the best-fit model at arbitrary X values
+          mdata - mfunc evaluated at the given X values
+          resids - The differences (y - mdata) 
+          rchisq - The reduced chi squared of the fit
+
+        You should also set any other fields that may be of use to someone
+        examining the fit results.
+        """
+        
+        raise NotImplementedError ()
+
+    def fit (self, guess=None):
+        guess = guess or self.guess ()
+
+        self.params = None
+        self.mfunc = None
+        self.mdata = None
+        self.resids = None
+        self.rchisq = None
+
+        if self.sigmas is None:
+            sig = _N.ones_like (self.x)
+        else:
+            sig = self.sigmas
+
+        self._fitImpl (self.x, self.y, sig, guess)
+
+        if self.params is None:
+            raise RuntimeError ('Failed to find best-fit parameters')
+        
+        if self.uncerts is None:
+            raise RuntimeError ('Failed to find uncertainties to fit parameters')
+
+        if len (self.params) != len (self._paramNames):
+            raise RuntimeError ('Should fit %d parameters; got %d' % (len (self._paramNames),
+                                                                      len (self.params)))
+        
+        if len (self.params) != len (self.uncerts):
+            raise RuntimeError ('Inconsistent params and uncerts fields')
+        
+        if self.mfunc is None:
+            self.mfunc = self.makeModel (*self.params)
+
+        if self.mdata is None:
+            self.mdata = self.mfunc (self.x)
+
+        if self.resids is None:
+            self.resids = self.y - self.mdata
+
+        if self.rchisq is None:
+            self.rchisq = (self.resids**2).sum () / (self.x.size - len (self.params))
+
+        return self
+
+    def printParams (self):
+        lmax = 0
+
+        for pn in self._paramNames:
+            if len (pn) > lmax: lmax = len (pn)
+                
+        for (pn, val, uncert) in zip (self._paramNames, self.params, self.uncerts):
+            frac = abs (100. * uncert / val)
+            print '%s: %14g +/- %14g (%.2f%%)' % (pn.rjust (lmax), val, uncert, frac)
+
+        return self
+
+    def plot (self, dlines=True, smoothModel=True):
+        import omega
+
+        if not smoothModel:
+            modx = self.x
+            mody = self.mdata
+        else:
+            modx = _N.linspace (self.x.min (), self.x.max (), 400)
+            mody = self.mfunc (modx)
+        
+        vb = omega.layout.VBox (2)
+        
+        vb[0] = omega.quickXY (self.x, self.y, 'Data', lines=dlines)
+        vb[0].addXY (modx, mody, 'Model')
+        vb[0].setYLabel ('Y')
+        vb[0].nudgeBounds (False, True)
+        
+        vb[1] = omega.RectPlot ()
+        vb[1].defaultField.xaxis = vb[1].defaultField.xaxis
+        vb[1].addXY (self.x, self.resids, 'O - C', lines=False)
+        vb[1].setLabels ('X', 'Residuals')
+        vb[1].nudgeBounds (False, True)
+        
+        vb.setWeight (0, 3)
+        return vb
+    
+class LinearFit (FitBase):
+    _paramNames = ['a', 'b']
+    
+    def guess (self):
+        return (0, 0)
+
+    def makeModel (self, a, b):
+        return lambda x: a + b * x
+
+    def _fitImpl (self, x, y, sig, guess):
+        # Ignore the guess since we can solve this exactly
+        # Exact solution math copied out of Numerical Recipes in C
+        # 2nd Ed. sec 15.2. (But no code copied)
+
+        sm1 = sig ** -1
+        sm2 = sm1 ** 2
+        
+        S = sm2.sum ()
+        Sx = (x * sm2).sum ()
+        Sy = (y * sm2).sum ()
+        Sxx = (x**2 * sm2).sum ()
+        Syy = (y**2 * sm2).sum ()
+        Sxy = (x * y * sm2).sum ()
+
+        D = S * Sxx - Sx**2
+
+        t = (x - Sx / S) * sm1
+        Stt = (t**2).sum ()
+        
+        self.b = (t * y * sm1).sum () / Stt
+        self.a = (Sy - Sx * self.b) / S
+        
+        self.sigma_a = (1 + Sx**2 / S / Stt) / S
+        self.sigma_b = Stt ** -1
+
+        self.params = _N.asarray ((self.a, self.b))
+        self.uncerts = _N.asarray ((self.sigma_a, self.sigma_b))
+
+class LeastSquaresFit (FitBase):
+    _fitExport = None
+    
+    def _fitImpl (self, x, y, sig, guess, **kwargs):
+        """Obtain a fit in some way, and set at least the following
+        fields:
+        
+        params - a tuple of best-fit parameters (compatible with the result of guess)
+        uncerts - A tuple of uncertainties of the parameters.
+        """
+
+        from scipy.optimize import leastsq
+        
+        w = sig ** -1
+        
+        def error (p):
+            self.mfunc = f = self.makeModel (*p)
+            return _N.ravel ((f (x) - y) * w)
+
+        pfit, cov, xx, msg, success = leastsq (error, guess, full_output=True,
+                                               **kwargs)
+
+        if success < 1 or success > 4:
+            raise Exception ('Least square fit failed: ' + msg)
+
+        self.params = pfit
+        self.uncerts = cov.diagonal ()
+
+        self.cov = cov
+
+        if self._fitExport is not None:
+            self._fitExport ()
+
+class GaussianFit (LeastSquaresFit):
+    _paramNames = ['height', 'xmid', 'width']
+    
+    def guess (self):
+        height = self.y.max ()
+
+        ytotal = self.y.sum ()
+        byx = self.y * self.x
+        xmid = byx.sum () / ytotal
+
+        # Not at all sure if this is a good algorithm. Seems to
+        # work OK.
+        width = _N.sqrt (abs ((self.x - xmid)**2 * self.y).sum () / ytotal)
+
+        return (height, xmid, width)
+
+    def makeModel (self, height, xmid, width):
+        return lambda x: height * _N.exp (-0.5 * ((x - xmid)/width)**2)
+
+    def _fitExport (self):
+        self.height, self.xmid, self.width = self.params
+        self.sigma_h, self.sigma_x, self.sigma_w = self.uncerts
+
+class DemoLinearFit (LeastSquaresFit):
+    _paramNames = ['a', 'b']
+    
+    def guess (self):
+        f = LinearFit ().setData (self.x, self.y, self.sigmas).fit ()
+        return f.params
+
+    def makeModel (self, a, b):
+        return lambda x: a + b * x
+
+    def _fitExport (self):
+        self.a, self.b = self.params
+        self.sigma_a, self.sigma_b = self.uncerts
