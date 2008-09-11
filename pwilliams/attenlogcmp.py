3b88842761840c8fb874e088a8852c0b63f1e83c
pkwill <pkwill@9b194dde-2b4d-0410-98f0-bba1668eb47e>
2008-09-11 18:16:45 +0000

    More user-friendly attenlogcmp.py
    git-svn-id: https://svn.hcro.org/mmm@110 9b194dde-2b4d-0410-98f0-bba1668eb47e

--- pwilliams/attenlogcmp.py
+++ pwilliams/attenlogcmp.py
@@ -2,33 +2,49 @@
 
 import sys
 
-def read (log):
+SVNID = '$Id$'
+CONTACT = 'Peter Williams <pwilliams@astro.berkeley.edu>'
+
+if len (sys.argv) < 2:
+    print 'Usage: %s autoatten.log1 [autoatten.log2 ...]' % sys.argv[0]
+    print 'Example: %s autoattenlog.*' % sys.argv[0]
+    print 'SVN Id:', SVNID
+    print 'Contact info:', CONTACT
+    sys.exit (1)
+
+def read (fn, log):
     res = {}
     
     for l in log:
-        a = l.strip ().split ()
-        ibob = a[1]
-        inp = a[2]
-        setting = float (a[3])
+        try:
+            l = l.strip ()
+            a = l.split ()
+            ibob = a[1]
+            inp = a[2]
+            setting = float (a[3])
 
-        if 'too low' in l:
-            rms = float (a[13])
-            desired = float (a[16])
-            flag = 'low'
-        else:
-            rms = float (a[7])
-            desired = float (a[10])
-            flag = 'ok'
+            if 'too low' in l:
+                rms = float (a[13])
+                desired = float (a[16])
+                flag = 'low'
+            else:
+                rms = float (a[7])
+                desired = float (a[10])
+                flag = 'ok'
 
-        name = '%s %s' % (ibob, inp)
-        res[name] = (setting, rms, desired, flag)
+            name = '%s %s' % (ibob, inp)
+            res[name] = (setting, rms, desired, flag)
+        except Exception, e:
+            print >>sys.stderr, 'Error parsing line in %s: "%s"' % (fn, l)
+            print >>sys.stderr, '   The exception is:', e
+            print >>sys.stderr, '   Ignoring the line.'
 
     return res
 
 tabs = {}
 
 for fn in sys.argv[1:]:
-    tabs[fn] = read (file (fn, 'r'))
+    tabs[fn] = read (fn, file (fn, 'r'))
 
 all = set ()
 for tab in tabs.itervalues ():
@@ -46,3 +62,5 @@ for name in sall:
 
         setting, rms, desired, flag = tab[name]
         print '   %20s: %6.1f %6.1f %6.1f %s' % (fn, setting, rms, desired, flag)
+
+sys.exit (0)
