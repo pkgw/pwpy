85ae7dc0eb378ce62f86b42a474fda3975e17282
pkwill <pkwill@9b194dde-2b4d-0410-98f0-bba1668eb47e>
2008-09-11 21:14:51 +0000

    Add a simple-as-possible scan script and datacatcher launcher wrapper.
    git-svn-id: https://svn.hcro.org/mmm@114 9b194dde-2b4d-0410-98f0-bba1668eb47e

---
+++ pwilliams/pyata/fxlaunch.sh
@@ -0,0 +1,16 @@
+#! /bin/sh -e
+
+src="$1"
+freq="$2"
+radec="$3"
+ndumps="$4"
+outbase="$5"
+antpols="$6"
+lo="$7"
+nsephem="$8"
+duration="$9"
+
+frfx64.csh `pwd` $freq $src $radec $ndumps &
+atafx $outbase-$src-$freq $antpols $lo $nsephem -duration $duration -noabort -fringeon
+wait
+exit $?
---
+++ pwilliams/pyscan.py
@@ -0,0 +1,49 @@
+#! /usr/bin/env python
+#
+# Do a quick scan with the ATA using the Python control routines.
+
+import sys
+from atactl import *
+import ataprobe
+
+me = 'pyscan'
+
+# Settings from the commandline
+
+if len (sys.argv) != 6:
+    print >>sys.stderr, 'Usage: %s [debug|real] instrument source freq durMins' % sys.argv[0]
+    print >>sys.stderr, 'E.g.: %s debug default 3c147 1430 1' % sys.argv[0]
+    sys.exit (1)
+
+if sys.argv[1] == 'real':
+    reallyDoIt = True
+elif sys.argv[1] == 'debug':
+    reallyDoIt = False
+else:
+    print >>sys.stderr, 'First argument must be "debug" or "real"; got', sys.argv[1]
+    sys.exit (1)
+
+instr = sys.argv[2]
+if instr == 'default': instr = None
+
+source = sys.argv[3]
+freq = int (sys.argv[4])
+durMins = int (sys.argv[5])
+
+# Do it!
+
+retcode = 1
+
+initScript (reallyDoIt, me + '.log')
+
+try:
+    h = ataprobe.Hookup (instr)
+    setIntegTime ()
+    lockServer ('lo' + h.lo)
+    observe (me, h, 'scan', source, freq, durMins * 60, True)
+    showAccounting ()
+    retcode = 0
+except Exception, e:
+    logAbort (e)
+    
+sys.exit (retcode)
