af0ec08710dd23f9844dd13da6eb608995f021d5
davidm <davidm@9b194dde-2b4d-0410-98f0-bba1668eb47e>
2009-02-04 23:04:55 +0000

    Added byte_size method (with bsize alias) to NArray class.
    git-svn-id: https://svn.hcro.org/mmm@269 9b194dde-2b4d-0410-98f0-bba1668eb47e

--- davidm/naptr/naptr.rb
+++ davidm/naptr/naptr.rb
@@ -13,3 +13,10 @@ require 'narray'
 naptr_shared_lib = 'naptr.' + Config::CONFIG['DLEXT']
 require naptr_shared_lib
 require 'naptr_gem' if false # Fake out RDoc
+
+class NArray
+  def byte_size
+    self.element_size * self.total
+  end
+  alias :bsize :byte_size
+end
