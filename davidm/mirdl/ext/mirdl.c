9add2ba9f05147f02e190e3748418c84706e7d35
davidm <davidm@9b194dde-2b4d-0410-98f0-bba1668eb47e>
2009-02-11 20:14:24 +0000

    Actually call headio.c init function.
    git-svn-id: https://svn.hcro.org/mmm@314 9b194dde-2b4d-0410-98f0-bba1668eb47e

--- davidm/mirdl/ext/mirdl.c
+++ davidm/mirdl/ext/mirdl.c
@@ -4,6 +4,7 @@ void init_mirdl_bug(VALUE mod);
 void init_mirdl_key(VALUE mod);
 void init_mirdl_options(VALUE mod);
 void init_mirdl_hio(VALUE mod);
+void init_mirdl_headio(VALUE mod);
 
 void Init_mirdl()
 {
@@ -12,4 +13,5 @@ void Init_mirdl()
   init_mirdl_key(mMirdl);
   init_mirdl_options(mMirdl);
   init_mirdl_hio(mMirdl);
+  init_mirdl_headio(mMirdl);
 }
