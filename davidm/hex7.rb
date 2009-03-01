688188222a61c771be2cba06e73c56fdc8108592
davidm <davidm@9b194dde-2b4d-0410-98f0-bba1668eb47e>
2009-03-01 05:28:57 +0000

    Fixed azel mode Removed dobsra/dobsdec and dantaz/dantel modes radec mode now does dobsra/dobsdec
    Output all fits for the same antpol together
    
    Added "offset" keyword.  offset=rect outputs pointing offset in rectangular
    form (arcmin, arcmin).  offset=polar outputs pointing offset in polar form
    (arcmin, degrees).
    
    
    git-svn-id: https://svn.hcro.org/mmm@379 9b194dde-2b4d-0410-98f0-bba1668eb47e

--- davidm/hex7.rb
+++ davidm/hex7.rb
@@ -20,13 +20,23 @@ end
 # Process command line
 keyini
 vis = mkeyf(:vis)
+
+valid_coords = %w[radec azel]
+coord = keya(:coord,valid_coords[0])
+
+valid_offsets = %w[rect polar]
+offset = keya(:offset,valid_offsets[0])
 keyfin
 
+bug('f',"coord must be one of #{valid_coords.inspect}") unless valid_coords.include? coord
+bug('f',"offset must be one of #{valid_offsets.inspect}") unless valid_offsets.include? offset
 bug('f','no datasets given (try "vis=...")') if vis.empty?
 bug('f',"need exactly 7 datasets (got #{vis.length})") unless vis.length == 7
 
 all_gains = {}
-all_pos = {}
+all_radec = {}
+all_azellst = {}
+lat = nil
 
 # For each dataset name given
 for dsname in vis
@@ -34,6 +44,9 @@ for dsname in vis
 
   # Open dataset
   tno = uvopen(dsname, :r)
+
+  # Get latitude (first time only)
+  lat ||= uvrdvrd(tno, :latitud)
 
   # Get gains from dataset
   ds_gains = get_gains(tno)
@@ -44,17 +57,26 @@ for dsname in vis
   # Get gains times for this dataset
   gain_times = ds_gains.keys.sort!
 
-  # Foreach gain time
+  # For each gain time
   for gt in gain_times
     # Scan through dataset until first dump after current gain time
     # (poor man's search algoithm)
     uvscan(tno, :time) until gt < uvrdvrd(tno, :time)
 
-    # Get sky position
-    # TODO Allow selection of radec, obsradec, antazel, azel
+    # Get obsra/obsdec position
+    radec = [:obsra, :obsdec].map {|v| uvrdvrd(tno, v)}
+
+    # Compute az/el
+    lst = uvrdvrd(tno, :lst)
+    azel = azel(radec[0], radec[1], lst, lat)
+
     # Each position is an angle, so store it as sin/cos components (i.e.
     # polar(1,x)) to prevent problems wrapping around north etc.
-    all_pos[gt] = [:ra, :dec].map {|v| uvrdvrd(tno, v).cis}
+    all_radec[gt] = radec.map {|radians| radians.cis}
+    all_azellst[gt] = azel.map {|radians| radians.cis}
+
+    # Store lst along with az,el.  No need to convert lst to cos/sin comonents
+    all_azellst[gt] << lst
   end
 end
 
@@ -66,10 +88,14 @@ STDERR.puts "got a total of #{nsols} solutions, #{nsols/7} hex patterns"
 
 ## Dump all data in time order
 #for t in all_times
-#  pos = all_pos[t]
 #  gains = all_gains[t]
-#  printf "%s %6.2f %6.2f %s\n", DateTime.ajd(t),
-#    pos[0].angle.r2d, pos[1].angle.r2d, gains.abs.to_a[0,4].inspect
+#  radec = all_radec[t]
+#  azellst = all_azellst[t]
+#  printf "%s %s %6.2f %6.2f %6.2f %6.2f %s\n",
+#    DateTime.ajd(t), azellst[2].r2h.to_hmsstr
+#    radec[0].angle.r2d, radec[1].angle.r2d,
+#    azellst[0].angle.r2d, azellst[1].angle.r2d,
+#    gains.abs.to_a[0,4].inspect
 #end
 
 # Hexes will store one array of fitted gaussian parameters per hex pattern.
@@ -80,20 +106,41 @@ hexes = []
 # Take positions and gain solutions seven at a time
 # (i.e. one hex pattern at a time)
 all_times.each_slice(7) do |hex_times|
-  pos7 = hex_times.map {|t| all_pos[t]}
   gains7 = hex_times.map {|t| all_gains[t]}
+  radec7 = hex_times.map {|t| all_radec[t]}
+  azellst7 = hex_times.map {|t| all_azellst[t]}
 
   # Calc delta pos in arcmin
   # TODO Do not assume first pointing is center
-  p0x, p0y = pos7[0].map {|z| z.conj}
-  dpos7 = pos7.map do |px, py|
-    dp = [px*p0x, py*p0y]
-    # Convert from cis to arcminutes
-    dp.map! {|c| c.angle * 180 * 60 / PI}
-    # Multiply dpx (e.g. dra) by cos(p0y) (e.g. cos(el0))
-    dp[0] *= p0y.real
-    dp
+  ra0, dec0 = radec7[0]
+  if coord == 'radec'
+    ra0, dec0 = ra0.conj, dec0.conj
+    dpos7 = radec7.map do |ra, dec|
+      # This is a small angle approximation.  If needed, ra/dec and ra0/dec0
+      # could be converted to vectors and their dot product taken
+      dp = [ra*ra0, dec*dec0]
+      # Convert from cis to arcminutes
+      dp.map! {|z| z.angle * 180 * 60 / PI}
+      # Multiply dra by cos(dec0)
+      dp[0] *= dec0.real
+      #p dp
+      dp
+    end
+  else
+    dpos7 = azellst7.map do |az, el, lst|
+      azel0 = azel(ra0.angle, dec0.angle, lst, lat).map {|radians| radians.cis.conj}
+      # This is a small angle approximation.  If needed, az/el and azel0
+      # could be converted to vectors and their dot product taken
+      dp = [az*azel0[0], el*azel0[1]]
+      # Convert from cis to arcminutes
+      dp.map! {|z| z.angle * 180 * 60 / PI}
+      # Multiply daz by cos(el0)
+      dp[0] *= azel0[1].real
+      #p dp
+      dp
+    end
   end
+  #exit
 
   # Calc inverse magnitude of all 7 gains arrays
   invgains7 = gains7.map do |gains|
@@ -108,12 +155,13 @@ all_times.each_slice(7) do |hex_times|
 
   # For each invgains7 element (i.e. antpol hex inverse gains)
   STDERR.puts "Fitting Gaussian to hex pattern #{hexes.length+1}"
-  hexes << invgains7.map do |aphex|
-    if aphex[0] != 0
+  hexes << invgains7.map do |aphexinvgains|
+    if aphexinvgains[0] != 0
       # fit 2d gaussian
-      iter, solver, status = Gauss2d.fit(dpos7, aphex, solver)
+      iter, solver, status = Gauss2d.fit(dpos7, aphexinvgains, solver)
       # dup stuff retuned from solver due to current GSL limitation/bug
-      [iter, solver.position.dup, solver.f.dup, solver.covar(0).dup*(solver.f.dnrm2**2/(7-5))]
+      apfit = [iter, solver.position.dup, solver.f.dup, solver.covar(0).dup*(solver.f.dnrm2**2/(7-5))]
+      apfit
       #p [iter, solver.position]
       #p solver.covar(0)*(solver.f.dnrm2**2/(7-5))
       #p solver.covar(0)
@@ -127,17 +175,22 @@ all_times.each_slice(7) do |hex_times|
 end
 
 # dump results for each hex pattern
-hexes.each_with_index do |hex, i|
-  puts "---- Gaussian fits for hex pattern #{i+1} ----"
-  hex.each_with_index do |(iters, gauss, chi, covar), ap|
-    ant = (ap >> 1) + 1
-    pol = (?X + (ap&1)).chr
+# TODO Print column header
+hexes.transpose.each_with_index do |fits, ap|
+  ant = (ap >> 1) + 1
+  pol = (?X + (ap&1)).chr
+  fits.each_with_index do |(iters, gauss, chi, covar), i|
     if iters.nil?
       #printf "%2d %s hex %d iters 0\n", ant, pol, i+1
       next
     end
-    printf "%2d %s hex %d iters %-2d :  %5.3f", ant, pol, i+1, iters, gauss[0]
-    gauss[1..-1].each {|x| printf "  %7.3f", x}
+    printf "%02d %s hex %d iters %-2d : %5.3f", ant, pol, i+1, iters, gauss[0]
+    if offset == 'polar'
+      r, th = Complex(*gauss[1,2]).polar
+      gauss[1] = r
+      gauss[2] = th.r2d
+    end
+    gauss[1..-1].each {|x| printf "  %8.3f", x}
     printf "  |X|= %.4f\n", chi.dnrm2
   end
 end
