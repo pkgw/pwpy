6ba83895ace01c8a10cc0e41b28a594424ac091c
claw <claw@9b194dde-2b4d-0410-98f0-bba1668eb47e>
2009-03-12 17:47:54 +0000

    added new pulsar imaging script
    git-svn-id: https://svn.hcro.org/mmm@416 9b194dde-2b4d-0410-98f0-bba1668eb47e

---
+++ claw/avgim.sh
@@ -0,0 +1,39 @@
+# average bm
+set i=0 ; maths exp='(<fxc-j0332-0.1s-xx-bin'${i}'aa.bm>+<fxc-j0332-0.1s-xx-bin'${i}'ab.bm>+<fxc-j0332-0.1s-yy-bin'${i}'aa.bm>+<fxc-j0332-0.1s-yy-bin'${i}'ab.bm>)/4' out=fxc-j0332-0.1s-i${i}.bm
+set i=1 ; maths exp='(<fxc-j0332-0.1s-xx-bin'${i}'aa.bm>+<fxc-j0332-0.1s-xx-bin'${i}'ab.bm>+<fxc-j0332-0.1s-yy-bin'${i}'aa.bm>+<fxc-j0332-0.1s-yy-bin'${i}'ab.bm>)/4' out=fxc-j0332-0.1s-i${i}.bm
+set i=2 ; maths exp='(<fxc-j0332-0.1s-xx-bin'${i}'aa.bm>+<fxc-j0332-0.1s-xx-bin'${i}'ab.bm>+<fxc-j0332-0.1s-yy-bin'${i}'aa.bm>+<fxc-j0332-0.1s-yy-bin'${i}'ab.bm>)/4' out=fxc-j0332-0.1s-i${i}.bm
+set i=3 ; maths exp='(<fxc-j0332-0.1s-xx-bin'${i}'aa.bm>+<fxc-j0332-0.1s-xx-bin'${i}'ab.bm>+<fxc-j0332-0.1s-yy-bin'${i}'aa.bm>+<fxc-j0332-0.1s-yy-bin'${i}'ab.bm>)/4' out=fxc-j0332-0.1s-i${i}.bm
+set i=4 ; maths exp='(<fxc-j0332-0.1s-xx-bin'${i}'aa.bm>+<fxc-j0332-0.1s-xx-bin'${i}'ab.bm>+<fxc-j0332-0.1s-yy-bin'${i}'aa.bm>+<fxc-j0332-0.1s-yy-bin'${i}'ab.bm>)/4' out=fxc-j0332-0.1s-i${i}.bm
+set i=5 ; maths exp='(<fxc-j0332-0.1s-xx-bin'${i}'aa.bm>+<fxc-j0332-0.1s-xx-bin'${i}'ab.bm>+<fxc-j0332-0.1s-yy-bin'${i}'aa.bm>+<fxc-j0332-0.1s-yy-bin'${i}'ab.bm>)/4' out=fxc-j0332-0.1s-i${i}.bm
+set i=6 ; maths exp='(<fxc-j0332-0.1s-xx-bin'${i}'aa.bm>+<fxc-j0332-0.1s-xx-bin'${i}'ab.bm>+<fxc-j0332-0.1s-yy-bin'${i}'aa.bm>+<fxc-j0332-0.1s-yy-bin'${i}'ab.bm>)/4' out=fxc-j0332-0.1s-i${i}.bm
+set i=7 ; maths exp='(<fxc-j0332-0.1s-xx-bin'${i}'aa.bm>+<fxc-j0332-0.1s-xx-bin'${i}'ab.bm>+<fxc-j0332-0.1s-yy-bin'${i}'aa.bm>+<fxc-j0332-0.1s-yy-bin'${i}'ab.bm>)/4' out=fxc-j0332-0.1s-i${i}.bm
+
+# average mp
+set i=0 ; maths exp='(<fxc-j0332-0.1s-xx-bin'${i}'aa.mp>+<fxc-j0332-0.1s-xx-bin'${i}'ab.mp>+<fxc-j0332-0.1s-yy-bin'${i}'aa.mp>+<fxc-j0332-0.1s-yy-bin'${i}'ab.mp>)/4' out=fxc-j0332-0.1s-i${i}.mp
+set i=1 ; maths exp='(<fxc-j0332-0.1s-xx-bin'${i}'aa.mp>+<fxc-j0332-0.1s-xx-bin'${i}'ab.mp>+<fxc-j0332-0.1s-yy-bin'${i}'aa.mp>+<fxc-j0332-0.1s-yy-bin'${i}'ab.mp>)/4' out=fxc-j0332-0.1s-i${i}.mp
+set i=2 ; maths exp='(<fxc-j0332-0.1s-xx-bin'${i}'aa.mp>+<fxc-j0332-0.1s-xx-bin'${i}'ab.mp>+<fxc-j0332-0.1s-yy-bin'${i}'aa.mp>+<fxc-j0332-0.1s-yy-bin'${i}'ab.mp>)/4' out=fxc-j0332-0.1s-i${i}.mp
+set i=3 ; maths exp='(<fxc-j0332-0.1s-xx-bin'${i}'aa.mp>+<fxc-j0332-0.1s-xx-bin'${i}'ab.mp>+<fxc-j0332-0.1s-yy-bin'${i}'aa.mp>+<fxc-j0332-0.1s-yy-bin'${i}'ab.mp>)/4' out=fxc-j0332-0.1s-i${i}.mp
+set i=4 ; maths exp='(<fxc-j0332-0.1s-xx-bin'${i}'aa.mp>+<fxc-j0332-0.1s-xx-bin'${i}'ab.mp>+<fxc-j0332-0.1s-yy-bin'${i}'aa.mp>+<fxc-j0332-0.1s-yy-bin'${i}'ab.mp>)/4' out=fxc-j0332-0.1s-i${i}.mp
+set i=5 ; maths exp='(<fxc-j0332-0.1s-xx-bin'${i}'aa.mp>+<fxc-j0332-0.1s-xx-bin'${i}'ab.mp>+<fxc-j0332-0.1s-yy-bin'${i}'aa.mp>+<fxc-j0332-0.1s-yy-bin'${i}'ab.mp>)/4' out=fxc-j0332-0.1s-i${i}.mp
+set i=6 ; maths exp='(<fxc-j0332-0.1s-xx-bin'${i}'aa.mp>+<fxc-j0332-0.1s-xx-bin'${i}'ab.mp>+<fxc-j0332-0.1s-yy-bin'${i}'aa.mp>+<fxc-j0332-0.1s-yy-bin'${i}'ab.mp>)/4' out=fxc-j0332-0.1s-i${i}.mp
+set i=7 ; maths exp='(<fxc-j0332-0.1s-xx-bin'${i}'aa.mp>+<fxc-j0332-0.1s-xx-bin'${i}'ab.mp>+<fxc-j0332-0.1s-yy-bin'${i}'aa.mp>+<fxc-j0332-0.1s-yy-bin'${i}'ab.mp>)/4' out=fxc-j0332-0.1s-i${i}.mp
+
+# cat together along third axis
+imcat in=fxc-j0332-0.1s-i"?".mp out=fxc-j0332-0.1s-icube.mp options=relax
+imcat in=fxc-j0332-0.1s-i"?".bm out=fxc-j0332-0.1s-icube.bm options=relax
+
+# arithmetic on mp
+avmaths in=fxc-j0332-0.1s-icube.mp out=fxc-j0332-0.1s-icube-sub.mp options=subtract
+
+# clean map (lightly) and restore
+clean map=fxc-j0332-0.1s-icube-sub.mp beam=fxc-j0332-0.1s-icube.bm out=fxc-j0332-0.1s-icube-sub.cl niters=100
+restor map=fxc-j0332-0.1s-icube-sub.mp beam=fxc-j0332-0.1s-icube.bm model=fxc-j0332-0.1s-icube-sub.cl out=fxc-j0332-0.1s-icube-sub.rm
+
+# fit the source!
+imfit in=fxc-j0332-0.1s-icube-sub.rm region='boxes(30,30,50,50)(2,2)' object=gaussian
+
+# redo cube without chan 2
+avmaths in=fxc-j0332-0.1s-icube.mp out=fxc-j0332-0.1s-icube-subno2.mp options=subtract region='image(1,1),image(3,8)'
+
+clean map=fxc-j0332-0.1s-icube-subno2.mp beam=fxc-j0332-0.1s-icube.bm out=fxc-j0332-0.1s-icube-subno2.cl niters=100
+restor map=fxc-j0332-0.1s-icube-subno2.mp beam=fxc-j0332-0.1s-icube.bm model=fxc-j0332-0.1s-icube-subno2.cl out=fxc-j0332-0.1s-icube-subno2.rm
