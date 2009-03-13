dfe5e9ec8a90b7d95ac233c8c98c7fb36e66055b
claw <claw@9b194dde-2b4d-0410-98f0-bba1668eb47e>
2009-03-13 18:54:48 +0000

    pulsar imaging closer to being scripted
    git-svn-id: https://svn.hcro.org/mmm@421 9b194dde-2b4d-0410-98f0-bba1668eb47e

--- claw/avgim.sh
+++ claw/avgim.sh
@@ -6,15 +6,16 @@
 #
 #
 #
-
-# set -e -x   # for debugging
-
-visroot='fxc-j0332-0.1s'
+##################
+imroot='j0332-0.1s'
 suffix='tst'
-halflist='aa'
+halflist='aa ab'
 phasebins=16
+##################
 
-for ((i=0; i<=${phasebines}-1; i++))
+#set -e -x   # for debugging
+
+for ((i=0; i<=${phasebins}-1; i++))
     do
     expmp=''
     expbm=''
@@ -22,38 +23,40 @@ for ((i=0; i<=${phasebines}-1; i++))
     # average bm and mp across pols and halves (split for 256 line limit of miriad)
     for half in $halflist
 	do
-	expmp='+<'${visroot}-xx-${suffix}'-bin'${i}${half}'.mp>+<'${visroot}-yy-${suffix}'-bin'${i}${half}'.mp>'${expmp}
-	expbm='+<'${visroot}-xx-${suffix}'-bin'${i}${half}'.bm>+<'${visroot}-yy-${suffix}'-bin'${i}${half}'.bm>'${expbm}
+	expmp='+<'${imroot}-xx-${suffix}'-bin'${i}${half}'.mp>+<'${imroot}-yy-${suffix}'-bin'${i}${half}'.mp>'${expmp}
+	expbm='+<'${imroot}-xx-${suffix}'-bin'${i}${half}'.bm>+<'${imroot}-yy-${suffix}'-bin'${i}${half}'.bm>'${expbm}
 	nhalves=`echo ${nhalves}+2 | bc`
     done
     # need to use cut to remove superfluous + symbol
     expmp=`echo $expmp | cut -c2-`
     expbm=`echo $expbm | cut -c2-`
+
+    # normalize by number of images
     expmp='('${expmp}')/'${nhalves}
     expbm='('${expbm}')/'${nhalves}
 
-    maths exp=${expmp} out=${visroot}'-i-'${suffix}'-bin'${i}$'.mp'
-    maths exp=${expbm} out=${visroot}'-i-'${suffix}'-bin'${i}$'.bm'
+    maths exp=${expmp} out=${imroot}'-i-'${suffix}'-bin'${i}$'.mp'
+    maths exp=${expbm} out=${imroot}'-i-'${suffix}'-bin'${i}$'.bm'
 done
 
 # cat together along third axis
-imcat in=${visroot}'-i-'${suffix}'-bin?.mp' out=${visroot}'-icube-'${suffix}'.mp' options=relax
-imcat in=${visroot}'-i-'${suffix}'-bin?.bm' out=${visroot}'-icube-'${suffix}'.bm' options=relax
+imcat in=${imroot}'-i-'${suffix}'-bin*.mp' out=${imroot}'-icube-'${suffix}'.mp' options=relax
+imcat in=${imroot}'-i-'${suffix}'-bin*.bm' out=${imroot}'-icube-'${suffix}'.bm' options=relax
 
 # arithmetic on mp
-avmaths in=${visroot}'-icube-'${suffix}'.mp' out=${visroot}'-icube-'${suffix}'-sub.mp' options=subtract
+avmaths in=${imroot}'-icube-'${suffix}'.mp' out=${imroot}'-icube-'${suffix}'-sub.mp' options=subtract
 
 echo
 echo '***Mean-subtracted image stats***'
-imstat in=${visroot}'-icube-'${suffix}'-sub.mp'
+imstat in=${imroot}'-icube-'${suffix}'-sub.mp'
 
 # clean map (lightly) and restore
-clean map=${visroot}'-icube-'${suffix}'-sub.mp' beam=${visroot}'-icube-'${suffix}'.bm' out=${visroot}'-icube-'${suffix}'-sub.cl' niters=100
-restor map=${visroot}'-icube-'${suffix}'-sub.mp' beam=${visroot}'-icube-'${suffix}'.bm' model=${visroot}'-icube-'${suffix}'-sub.cl' out=${visroot}'-icube-'${suffix}'-sub.rm'
+clean map=${imroot}'-icube-'${suffix}'-sub.mp' beam=${imroot}'-icube-'${suffix}'.bm' out=${imroot}'-icube-'${suffix}'-sub.cl' niters=100
+restor map=${imroot}'-icube-'${suffix}'-sub.mp' beam=${imroot}'-icube-'${suffix}'.bm' model=${imroot}'-icube-'${suffix}'-sub.cl' out=${imroot}'-icube-'${suffix}'-sub.rm'
 
 echo
 echo '***Restored image stats***'
-imstat in=${visroot}'-icube-'${suffix}'-sub.rm'
+imstat in=${imroot}'-icube-'${suffix}'-sub.rm'
 
 # other analysis
 #
--- claw/icr-bin.sh
+++ claw/icr-bin.sh
@@ -8,25 +8,28 @@
 # *.bm Beam
 #
 # Assumes data with no autos or cross-hand pol
+#
+##################
+visroot='fxc-j0332-0.1s'
+imroot='j0332-0.1s'
+suffix='tst'
+halflist='aa ab'     # data split?  half = aa, ab, ac, ...
+phasebins=16
+##################
 
 #set -e -x  # for debugging
-
-visroot='fxc-j0332-0.1s'
-suffix='tst'
-halflist='aa'     # data split?  half = aa, ab, ac, ...
 file='time-'${suffix}
-phasebins=16
 
 for half in ${halflist}
   do
 
   for ((i=0; i<=${phasebins}-1; i++))
     do
-    mpsmall=${visroot}'-xx-'${suffix}'-bin'${i}${half}.mp
-    bmsmall=${visroot}'-xx-'${suffix}'-bin'${i}${half}.bm
+    mpsmall=${imroot}'-xx-'${suffix}'-bin'${i}${half}.mp
+    bmsmall=${imroot}'-xx-'${suffix}'-bin'${i}${half}.bm
     invert vis=${visroot}'-xx' map=$mpsmall beam=$bmsmall select='@'${file}'-bin'${i}${half} options=double,mfs sup=0 imsize=78,78
-    mpsmall=${visroot}'-yy-'${suffix}'-bin'${i}${half}.mp
-    bmsmall=${visroot}'-yy-'${suffix}'-bin'${i}${half}.bm
+    mpsmall=${imroot}'-yy-'${suffix}'-bin'${i}${half}.mp
+    bmsmall=${imroot}'-yy-'${suffix}'-bin'${i}${half}.bm
     invert vis=${visroot}'-yy' map=$mpsmall beam=$bmsmall select='@'${file}'-bin'${i}${half} options=double,mfs sup=0 imsize=78,78
   done
 
@@ -35,8 +38,8 @@ for half in ${halflist}
   for ((i=0; i<=${phasebins}-1; i++))
     do
     echo 'Image '${i}':'
-    imstat in=${visroot}'-xx-'${suffix}'-bin'${i}${half}.mp | tail -n 2
-    imstat in=${visroot}'-yy-'${suffix}'-bin'${i}${half}.mp | tail -n 2
+    imstat in=${imroot}'-xx-'${suffix}'-bin'${i}${half}.mp | tail -n 2
+    imstat in=${imroot}'-yy-'${suffix}'-bin'${i}${half}.mp | tail -n 2
   done
 
 done
