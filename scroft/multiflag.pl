1a472613fe25071d685d673458d64429632253e1
scroft <scroft@9b194dde-2b4d-0410-98f0-bba1668eb47e>
2009-04-10 19:58:28 +0000

    Added multiflag.pl
    git-svn-id: https://svn.hcro.org/mmm@483 9b194dde-2b4d-0410-98f0-bba1668eb47e

---
+++ scroft/multiflag.pl
@@ -0,0 +1,48 @@
+#!/usr/bin/env perl
+# syntax: multiflag.pl filename
+# E.g. to apply flags to all mosfxc-* in cwd:
+# multiflag.pl mosfxc-* | csh
+#
+# SDC 2009/04/08
+# Modified to flag groups of antennas
+
+# file containing bad antenna numbers, one per line
+$badf="badants";
+
+open(BADF,$badf);
+@badants=<BADF>;
+close(BADF);
+
+# need to break down flagging commands into less than ~50 characters
+# so MIRIAD doesn't choke on too long a line
+$antfbatch = 0;
+$antflags[$antfbatch] = "\"ant(";
+foreach $badant (@badants) {
+    chomp ($badant);
+    $antflags[$antfbatch] = "$antflags[$antfbatch]$badant,";
+    if (length($antflags[$antfbatch]) > 50) {
+	# this flagging command is getting too big, so start a new one
+	chop($antflags[$antfbatch]);
+	$antflags[$antfbatch]="$antflags[$antfbatch])\"";
+	$antfbatch+=1;
+	$antflags[$antfbatch] = "\"ant(";
+    }
+}
+
+# close off the last flagging command
+chop($antflags[$antfbatch]);
+$antflags[$antfbatch]="$antflags[$antfbatch])\"";
+
+if ($antflags[$antfbatch] eq "\"ant)\"") {
+    # last flagging command had nothing in it
+    delete($antflags[$antfbatch]);
+}
+    
+# loop through files from the command line
+foreach $file (@ARGV) {
+    # loop through each flagging command
+    foreach $flagcom (@antflags) {
+	# output flagging commands
+	print("uvflag vis=$file flagval=f select=$flagcom\n");
+    }
+}
