61fbc9156d0f38711996f4efa36ee441d86a5286
claw <claw@9b194dde-2b4d-0410-98f0-bba1668eb47e>
2009-05-08 18:10:01 +0000

    updating plotting for many inputs
    git-svn-id: https://svn.hcro.org/mmm@503 9b194dde-2b4d-0410-98f0-bba1668eb47e

--- claw/plotleak-realimag.py
+++ claw/plotleak-realimag.py
@@ -6,45 +6,24 @@ import asciidata, pylab, numpy
 
 def run():
     # params
-    type = 'xy'
+    type = 'test'
 
-    a1 = asciidata.open('3c286-leak1.amp.txt')
-    p1 = asciidata.open('3c286-leak1.phase.txt')
-    a2 = asciidata.open('3c286-leak2.amp.txt')
-    p2 = asciidata.open('3c286-leak2.phase.txt')
-    a3 = asciidata.open('3c286-leak3.amp.txt')
-    p3 = asciidata.open('3c286-leak3.phase.txt')
-    a4 = asciidata.open('3c286-leak4.amp.txt')
-    p4 = asciidata.open('3c286-leak4.phase.txt')
-    a5 = asciidata.open('3c286-leak5.amp.txt')
-    p5 = asciidata.open('3c286-leak5.phase.txt')
-    a6 = asciidata.open('3c286-leak6.amp.txt')
-    p6 = asciidata.open('3c286-leak6.phase.txt')
-    a7 = asciidata.open('3c286-leak7.amp.txt')
-    p7 = asciidata.open('3c286-leak7.phase.txt')
-    a8 = asciidata.open('3c286-leak8.amp.txt')
-    p8 = asciidata.open('3c286-leak8.phase.txt')
+# funky variable iterating (doesn't work when run as script)
+#    l = locals()
+#    for i in range(8):
+#        l['a'+str(i)] = asciidata.open('3c286-leak1.amp.txt')
+#        l['p'+str(i)] = asciidata.open('3c286-leak1.phase.txt')
 
-    a12 = asciidata.open('3c138-leak1.amp.txt')
-    p12 = asciidata.open('3c138-leak1.phase.txt')
-    a22 = asciidata.open('3c138-leak2.amp.txt')
-    p22 = asciidata.open('3c138-leak2.phase.txt')
-    a32 = asciidata.open('3c138-leak3.amp.txt')
-    p32 = asciidata.open('3c138-leak3.phase.txt')
-    a42 = asciidata.open('3c138-leak4.amp.txt')
-    p42 = asciidata.open('3c138-leak4.phase.txt')
-    a52 = asciidata.open('3c138-leak5.amp.txt')
-    p52 = asciidata.open('3c138-leak5.phase.txt')
-    a62 = asciidata.open('3c138-leak6.amp.txt')
-    p62 = asciidata.open('3c138-leak6.phase.txt')
-    a72 = asciidata.open('3c138-leak7.amp.txt')
-    p72 = asciidata.open('3c138-leak7.phase.txt')
-    a82 = asciidata.open('3c138-leak8.amp.txt')
-    p82 = asciidata.open('3c138-leak8.phase.txt')
+    a = []
+    for i in range(8):
+        a.append(asciidata.open ('3c286-leak%d.amp.txt' % i))
+        p.append(asciidata.open ('3c286-leak%d.phase.txt' % i))
+        a2.append(asciidata.open ('3c138-leak%d.amp.txt' % i))
+        p2.append(asciidata.open ('3c138-leak%d.phase.txt' % i))
 
     # group 3c286
     # original phase and amplitude
-    px = numpy.array([p1.columns[0],p2.columns[0],p3.columns[0],p4.columns[0],p5.columns[0],p6.columns[0],p7.columns[0],p8.columns[0]])
+    px = numpy.array([p[0].columns[0],p[1].columns[0],p[2].columns[0],p[3].columns[0],p[4].columns[0],p[5].columns[0],p[6].columns[0],p[7].columns[0]])
     ax = numpy.array([a1.columns[0],a2.columns[0],a3.columns[0],a4.columns[0],a5.columns[0],a6.columns[0],a7.columns[0],a8.columns[0]])
     py = numpy.array([p1.columns[1],p2.columns[1],p3.columns[1],p4.columns[1],p5.columns[1],p6.columns[1],p7.columns[1],p8.columns[1]])
     ay = numpy.array([a1.columns[1],a2.columns[1],a3.columns[1],a4.columns[1],a5.columns[1],a6.columns[1],a7.columns[1],a8.columns[1]])
@@ -83,12 +62,24 @@ def run():
         for i in range(len(px[0])):
             pylab.figure(1)
             pylab.plot(rx[:,i],ix[:,i],'.-')
+            pylab.xlabel('Real')
+            pylab.ylabel('Imaginary')
+            pylab.title('Leakages')
             pylab.figure(2)
             pylab.plot(ry[:,i],iy[:,i],'.-')
+            pylab.xlabel('Real')
+            pylab.ylabel('Imaginary')
+            pylab.title('Leakages')
             pylab.figure(3)
             pylab.plot(rx2[:,i],ix2[:,i],'.-')
+            pylab.xlabel('Real')
+            pylab.ylabel('Imaginary')
+            pylab.title('Leakages')
             pylab.figure(4)
             pylab.plot(ry2[:,i],iy2[:,i],'.-')
+            pylab.xlabel('Real')
+            pylab.ylabel('Imaginary')
+            pylab.title('Leakages')
 
     pylab.show()
 
