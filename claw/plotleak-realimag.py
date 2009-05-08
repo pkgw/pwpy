0464a2279e9ecb7751eb063df346271750d32b98
claw <claw@9b194dde-2b4d-0410-98f0-bba1668eb47e>
2009-05-08 19:16:39 +0000

    finished updating plotting for many inputs
    git-svn-id: https://svn.hcro.org/mmm@504 9b194dde-2b4d-0410-98f0-bba1668eb47e

--- claw/plotleak-realimag.py
+++ claw/plotleak-realimag.py
@@ -6,7 +6,7 @@ import asciidata, pylab, numpy
 
 def run():
     # params
-    type = 'test'
+    type = 'xy'
 
 # funky variable iterating (doesn't work when run as script)
 #    l = locals()
@@ -14,8 +14,9 @@ def run():
 #        l['a'+str(i)] = asciidata.open('3c286-leak1.amp.txt')
 #        l['p'+str(i)] = asciidata.open('3c286-leak1.phase.txt')
 
-    a = []
-    for i in range(8):
+    a = []; a2 = []
+    p = []; p2 = []
+    for i in range(1,9):
         a.append(asciidata.open ('3c286-leak%d.amp.txt' % i))
         p.append(asciidata.open ('3c286-leak%d.phase.txt' % i))
         a2.append(asciidata.open ('3c138-leak%d.amp.txt' % i))
@@ -24,9 +25,9 @@ def run():
     # group 3c286
     # original phase and amplitude
     px = numpy.array([p[0].columns[0],p[1].columns[0],p[2].columns[0],p[3].columns[0],p[4].columns[0],p[5].columns[0],p[6].columns[0],p[7].columns[0]])
-    ax = numpy.array([a1.columns[0],a2.columns[0],a3.columns[0],a4.columns[0],a5.columns[0],a6.columns[0],a7.columns[0],a8.columns[0]])
-    py = numpy.array([p1.columns[1],p2.columns[1],p3.columns[1],p4.columns[1],p5.columns[1],p6.columns[1],p7.columns[1],p8.columns[1]])
-    ay = numpy.array([a1.columns[1],a2.columns[1],a3.columns[1],a4.columns[1],a5.columns[1],a6.columns[1],a7.columns[1],a8.columns[1]])
+    ax = numpy.array([a[0].columns[0],a[1].columns[0],a[2].columns[0],a[3].columns[0],a[4].columns[0],a[5].columns[0],a[6].columns[0],a[7].columns[0]])
+    py = numpy.array([p[0].columns[1],p[1].columns[1],p[2].columns[1],p[3].columns[1],p[4].columns[1],p[5].columns[1],p[6].columns[1],p[7].columns[1]])
+    ay = numpy.array([a[0].columns[1],a[1].columns[1],a[2].columns[1],a[3].columns[1],a[4].columns[1],a[5].columns[1],a[6].columns[1],a[7].columns[1]])
     # derived r and i
     rx = ax * numpy.cos(numpy.radians(px))
     ix = ax * numpy.sin(numpy.radians(px))
@@ -35,10 +36,10 @@ def run():
 
     # group 3c138
     # original phase and amplitude
-    px2 = numpy.array([p12.columns[0],p22.columns[0],p32.columns[0],p42.columns[0],p52.columns[0],p62.columns[0],p72.columns[0],p82.columns[0]])
-    ax2 = numpy.array([a12.columns[0],a22.columns[0],a32.columns[0],a42.columns[0],a52.columns[0],a62.columns[0],a72.columns[0],a82.columns[0]])
-    py2 = numpy.array([p12.columns[1],p22.columns[1],p32.columns[1],p42.columns[1],p52.columns[1],p62.columns[1],p72.columns[1],p82.columns[1]])
-    ay2 = numpy.array([a12.columns[1],a22.columns[1],a32.columns[1],a42.columns[1],a52.columns[1],a62.columns[1],a72.columns[1],a82.columns[1]])
+    px2 = numpy.array([p2[0].columns[0],p2[1].columns[0],p2[2].columns[0],p2[3].columns[0],p2[4].columns[0],p2[5].columns[0],p2[6].columns[0],p2[7].columns[0]])
+    ax2 = numpy.array([a2[0].columns[0],a2[1].columns[0],a2[2].columns[0],a2[3].columns[0],a2[4].columns[0],a2[5].columns[0],a2[6].columns[0],a2[7].columns[0]])
+    py2 = numpy.array([p2[0].columns[1],p2[1].columns[1],p2[2].columns[1],p2[3].columns[1],p2[4].columns[1],p2[5].columns[1],p2[6].columns[1],p2[7].columns[1]])
+    ay2 = numpy.array([a2[0].columns[1],a2[1].columns[1],a2[2].columns[1],a2[3].columns[1],a2[4].columns[1],a2[5].columns[1],a2[6].columns[1],a2[7].columns[1]])
     # derived r and i
     rx2 = ax2 * numpy.cos(numpy.radians(px2))
     ix2 = ax2 * numpy.sin(numpy.radians(px2))
@@ -58,7 +59,8 @@ def run():
             pylab.polar(numpy.radians(py2[:,i]),ay2[:,i],'.-')
 
     if type == 'xy':
-    # real-imag (x-y) plot
+        # real-imag (x-y) plot
+        # two per source
         for i in range(len(px[0])):
             pylab.figure(1)
             pylab.plot(rx[:,i],ix[:,i],'.-')
