8ff2c9d8fdeb4d893b807ae9a83a881a7724107b
claw <claw@9b194dde-2b4d-0410-98f0-bba1668eb47e>
2009-10-13 22:39:41 +0000

    script to plot rm in hammer-aitoff projection
    git-svn-id: https://svn.hcro.org/mmm@690 9b194dde-2b4d-0410-98f0-bba1668eb47e

---
+++ claw/rmhammer.py
@@ -0,0 +1,22 @@
+import numpy as n
+import pylab as p
+import asciidata
+
+cat = asciidata.AsciiData('/Users/claw/data/nvss-rm/RMCatalogue.txt')
+pol = n.array(cat.columns[15])
+good = n.where(pol > 200)[0]
+rm = n.array(cat.columns[21])[good]
+l = n.array(cat.columns[10])[good]
+b = n.array(cat.columns[11])[good]
+
+p.subplot(111, projection="hammer")
+p.grid()
+p.xticks(n.radians(n.arange(-150,180,30)),('150','120','90','60','30','0','-30','-60','-90','-120','-150','-180'))
+
+for i in range(len(rm)):
+    print l[i], b[i], pol[good][i], rm[i]
+    if rm[i] > 0.0:
+        p.plot(n.radians([-1*l[i]]), n.radians([b[i]]), 'o', markersize=abs(rm[i]/3.), color='red', alpha=0.5)
+    else:
+        p.plot(n.radians([-1*l[i]]), n.radians([b[i]]), 'x', markersize=abs(rm[i]/3.), color='blue', alpha=0.5)
+
