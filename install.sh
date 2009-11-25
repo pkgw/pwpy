6af76aee23d76df28917581600dc97d743c30209
peter <peter@69fc08fc-fdfc-0310-ace8-9e8da0474e22>
2009-11-25 03:20:53 +0000

    pylib: add quickie script for installation
    git-svn-id: https://newton.cx/svn/trunk/pylib@1295 69fc08fc-fdfc-0310-ace8-9e8da0474e22

---
+++ install.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+if [ ! -f instdest ] ; then
+    echo "Create a file named 'instdest' specifying the install destination." >&2
+    exit 1
+fi
+
+dest=`cat instdest`
+dest=`eval echo $dest`
+set -e -x
+cp -v *.py *.glade $dest
