4817bfdc6c442fb7f7d43d9223f95a44ceb1ae80
pkwill <pkwill@9b194dde-2b4d-0410-98f0-bba1668eb47e>
2009-11-26 01:53:31 +0000

    Add little script for installing scripts somewhere
    git-svn-id: https://svn.hcro.org/mmm@742 9b194dde-2b4d-0410-98f0-bba1668eb47e

---
+++ pwilliams/install_pw_mmm_stuff.sh
@@ -0,0 +1,75 @@
+#! /bin/bash
+#
+# Install the scripts and Python modules in MMM/pwilliams into other directories;
+# this helps reduce the proliferation of $PYTHONPATH entries, at the expense of
+# having to remember to reinstall.
+
+if [ ! -f instpfx ] ; then
+    echo "Create a file called 'instpfx' with the install prefix." >&2
+    exit 1
+fi
+
+pfx=`cat instpfx`
+pfx=`eval echo $pfx`
+
+programs='
+atastdflag
+C
+chbo
+icr.sh
+micr.sh
+mirargmerge
+pg2eps
+polsplit
+pwflag
+show
+sortinplace
+tiph
+uvdam
+uvdph
+uvlock
+uvlwcp
+uvlwrevert
+uvrevert
+walsh_adc_conflict.sh
+walsh-flags.py
+fancy/ampadd.py
+fancy/amprfi
+fancy/atabpass
+fancy/ataglue.py
+fancy/byant
+fancy/calctsys
+fancy/closanal
+fancy/copytsys
+fancy/fxcal.py
+fancy/gpcat
+fancy/gpunity
+fancy/multiflag2
+fancy/qimage
+fancy/uvasimg
+fancy/uvpyextract
+fancy/varcat.py
+'
+
+modules='
+pyata/atactl.py
+pyata/ataprobe.py
+pyata/calibrators.py
+pyata/fxlaunch.sh
+pyata/rfi.py
+pylib/amprfi.py
+pylib/ata2mir.py
+pylib/atabpass.py
+pylib/blflag.py
+pylib/blmapper.glade
+pylib/calctsys.py
+pylib/closanal.py
+pylib/hhaa.dat
+pylib/multiflag.py
+pylib/multiflag2.py
+pylib/uvextract.py
+'
+
+set -e
+cp -vp $programs $pfx/bin
+cp -vp $modules $pfx/lib/python/site-packages
