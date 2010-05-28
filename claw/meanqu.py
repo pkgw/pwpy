b04de86ec7c5395f951eec89de2e93d510d29657
claw <claw@9b194dde-2b4d-0410-98f0-bba1668eb47e>
2010-05-28 21:40:37 +0000

    rmsynth paper plotting
    git-svn-id: https://svn.hcro.org/mmm@836 9b194dde-2b4d-0410-98f0-bba1668eb47e

--- claw/meanqu.py
+++ claw/meanqu.py
@@ -2,7 +2,7 @@
 
 import sys
 
-if len(sys.argv) != 2:
+if len(sys.argv) < 2:
     print 'dude, give me a log file.  wtf?'
     exit(1)
 
@@ -16,6 +16,18 @@ q = numpy.array(f.columns[1])
 u = numpy.array(f.columns[2])
 err = numpy.array(f.columns[3])
 
-print numpy.sum(q/err**2)/numpy.sum(1/err**2)
-print numpy.sum(u/err**2)/numpy.sum(1/err**2)
-print 1/numpy.sqrt(numpy.sum(1/err**2))
+# flux model for stokes parameters
+trueq = 0.6285 * (1.0035/nu)**(-0.359)
+trueu = 1.4119 * (1.0035/nu)**(-0.359)
+
+print q-trueq
+print u-trueu
+
+# fluxweight = numpy.sum(q/err**2)/numpy.sum(1/err**2)
+# print 1/numpy.sqrt(numpy.sum(1/err**2))
+meanq = (q-trueq).mean()
+meanu = (u-trueu).mean()
+stdq = (q-trueq).std()
+stdu = (u-trueu).std()
+
+print '%2f\pm%2f,%2f\pm%2f' % (1000*meanq,1000*stdq,1000*meanu,1000*stdu) 
--- claw/rmhammer.py
+++ claw/rmhammer.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 """
 claw, 13oct09
 Script to plot RM values from catalog in Hammer-Aitoff (equal area), all-sky projection.
@@ -8,10 +10,15 @@ import numpy as n
 import pylab as p
 import asciidata
 
-cat = asciidata.AsciiData('/indirect/big_scr3/claw/data/ata/nvss-rm-best/log-merged/tmp')
-rm = n.array(cat.columns[9])
-l = n.array(cat.columns[1])
-b = n.array(cat.columns[2])
+cat = asciidata.AsciiData('/indirect/big_scr3/claw/data/ata/nvss-rm-best/rm-lb-lowres.txt')
+rm = n.array(cat.columns[1])
+l = n.array(cat.columns[3])
+b = n.array(cat.columns[4])
+
+center = n.where(abs(rm) <= 600)
+rm = rm[center]
+l = l[center]
+b = b[center]
 
 # cat = asciidata.AsciiData('/Users/claw/data/nvss-rm/RMCatalogue.txt')
 #pol = n.array(cat.columns[15])
@@ -27,8 +34,8 @@ p.xticks(n.radians(n.arange(-150,180,30)),('150','120','90','60','30','0','-30',
 for i in range(len(rm)):
     print l[i], b[i], rm[i]
     if rm[i] > 0.0:
-        p.plot(n.radians([-1*l[i]]), n.radians([b[i]]), 'o', markersize=abs(rm[i]/2.), color='red', alpha=0.9)
+        p.plot(n.radians([-1*l[i]]), n.radians([b[i]]), 'o', markersize=5+(abs(rm[i]/5.)), color='red', alpha=0.7)
     else:
-        p.plot(n.radians([-1*l[i]]), n.radians([b[i]]), 'x', markersize=abs(rm[i]/2.), color='blue', alpha=0.9)
+        p.plot(n.radians([-1*l[i]]), n.radians([b[i]]), 'x', markersize=5+(abs(rm[i]/5.)), color='blue', alpha=1.0)
 
 p.show()
