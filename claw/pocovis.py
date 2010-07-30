11c195c3675a4950a31646b94877d0274fcaffbb
claw <claw@9b194dde-2b4d-0410-98f0-bba1668eb47e>
2010-07-30 20:59:16 +0000

    made pocovis class-like and added dispersion mask generator
    git-svn-id: https://svn.hcro.org/mmm@877 9b194dde-2b4d-0410-98f0-bba1668eb47e

--- claw/pocovis.py
+++ claw/pocovis.py
@@ -9,112 +9,158 @@
 """
 
 import sys
-import mirtask
-from mirtask import uvdat, keys, util
+#import mirtask
+#from mirtask import uvdat, keys, util
+import miriad
 import numpy as n
 import pylab as p
 import scipy.optimize as opt
 from matplotlib.font_manager import fontManager, FontProperties
 
-# This has us get our input file from the vis= keyword
-# with support for select=, line=, stokes=,
-# options=nocal,nopass,nopol
-
-# initialize'
-file = 'poco_crab_candpulse.mir'
-sys.argv.append('vis='+file)
-nchan = 64
-chans = n.arange(10,50)
-nbl = 36
-sfreq = 0.77  # freq for first channel in GHz
-sdf = 0.104/nchan   # dfreq per channel in GHz
-baseline_order = n.array([ 257, 258, 514, 261, 517, 1285, 262, 518, 1286, 1542, 259, 515, 773, 774, 771, 516, 1029, 1030, 772, 1028, 1287, 1543, 775, 1031, 1799, 1544, 776, 1032, 1800, 2056, 260, 263, 264, 519, 520, 1288])   # second iteration of bl nums
-#baseline_order = n.array([257, 258, 514, 259, 515, 771, 260, 516, 772, 1028, 261, 517, 773, 1029, 1285, 518, 774, 1030, 1286, 1542, 775, 1031, 1287, 1543, 1799, 1032, 1288, 1544, 1800, 2056, 262, 263, 264, 519, 520, 776])   # wrong first guess?
-delay = n.array([0.,0.,0.,0.,0.,0.,0.,0.])  # first guess at delays in ns
-#delay = n.array([0.,0.,-20.,-20.,0.,0.,20.,20.])  # first guess at delays in noise source data (tfl8)
 font= FontProperties(size='x-small');
-autos = []
-noautos = []
-for a1 in range(1,9):             # loop to adjust delays
-    for a2 in range(a1,9):
-        blindex = n.where(baseline_order == a1*256 + a2)[0][0]
-        if a1 == a2:
-            autos.append(blindex)
-        else:
-            noautos.append(blindex)
 
-def load():
-# read in data and create arrays
-    keys.doUvdat ('dsl3', True)
-    opts = keys.process ()
+class poco:
+    def __init__(self):
+        # initialize'
+        self.nchan = 64
+        self.chans = n.arange(10,50)
+        self.nbl = 36
+        self.sfreq = 0.77  # freq for first channel in GHz
+        self.sdf = 0.104/self.nchan   # dfreq per channel in GHz
+        self.baseline_order = n.array([ 257, 258, 514, 261, 517, 1285, 262, 518, 1286, 1542, 259, 515, 773, 774, 771, 516, 1029, 1030, 772, 1028, 1287, 1543, 775, 1031, 1799, 1544, 776, 1032, 1800, 2056, 260, 263, 264, 519, 520, 1288])   # second iteration of bl nums
+        self.autos = []
+        self.noautos = []
+        for a1 in range(1,9):             # loop to adjust delays
+            for a2 in range(a1,9):
+                self.blindex = n.where(self.baseline_order == a1*256 + a2)[0][0]
+                if a1 == a2:
+                    self.autos.append(self.blindex)
+                else:
+                    self.noautos.append(self.blindex)
 
-    i = 0
-    initsize=50000
-    da = n.zeros((initsize,nchan),dtype='complex64')
-    fl = n.zeros((initsize,nchan),dtype='complex64')
-    ti = n.zeros((initsize),dtype='complex64')
-    for inp, preamble, data, flags, nread in uvdat.readAll ():
+    def load(self,file):
+# slick way, but hard to hack for interactive use
+#        sys.argv.append('vis='+file)
+#        keys.doUvdat ('dsl3', True)
+#        opts = keys.process ()
 
-   # Reduce these arrays to the correct size
-        data = data[0:nread]
-        flags = flags[0:nread]
+# poor man's way
+        vis = miriad.VisData(file)
+
+        # initialize parameters
+        nchan = self.nchan
+        nbl = self.nbl
+        i = 0
+        initsize=50000
+        da = n.zeros((initsize,nchan),dtype='complex64')
+        fl = n.zeros((initsize,nchan),dtype='bool')
+        ti = n.zeros((initsize),dtype='float64')
+
+        # read data
+        # You can pass traditional Miriad UV keywords to readLowlevel as keyword arguments
+        for inp, preamble, data, flags, nread in vis.readLowlevel (False):
+#        for inp, preamble, data, flags, nread in uvdat.readAll ():
+            # Reduce these arrays to the correct size
+            data = data[0:nread]
+            flags = flags[0:nread]
 
     # Decode the preamble
-#        u, v, w = preamble[0:3]
-        time = preamble[3]
-#        baseline = util.decodeBaseline (preamble[4])
+    #        u, v, w = preamble[0:3]
+            time = preamble[3]
+    #        baseline = util.decodeBaseline (preamble[4])
 
-#    pol = uvdat.getPol ()
+    #    pol = uvdat.getPol ()
     
-        if i < initsize:
-            ti[i] = time
-            da[i] = data
-            fl[i] = flags
-        else:
-            da = n.concatenate((da,[data]))
-            ti = n.concatenate((ti,[time]))
-            fl = n.concatenate((fl,[flags]))
-#            bl = n.concatenate((bl,[baseline]))
-        i = i+1
+            if i < initsize:
+                ti[i] = time
+                da[i] = data
+                fl[i] = flags
+            else:
+                da = n.concatenate((da,[data]))
+                ti = n.concatenate((ti,[time]))
+                fl = n.concatenate((fl,[flags]))
+        #            bl = n.concatenate((bl,[baseline]))
+            i = i+1
 
-    if i <= initsize:
-        da = da[0:i]
-        fl = fl[0:i]
-        ti = ti[0:i]
+        if i <= initsize:
+            print 'Array smaller than initialized array.  Trimming.'
+            da = da[0:i]
+            fl = fl[0:i]
+            ti = ti[0:i]
 
-    da = da.reshape(i/nbl,nbl,nchan)
-    fl = fl.reshape(i/nbl,nbl,nchan)
-    ti = ti[::nbl]
-    return ti,da,fl
-
-# End of loop.
-
-def debug(da):
-    for a1 in range(1,2):             # loop to adjust delays
-        for a2 in range(a1,9):
-            blindex = n.where(baseline_order == a1*256 + a2)[0][0]
-            print 'For int 0, a1,a2,blindex,baseline:', a1, a2, blindex, a1*256 + a2
-            print da[0,blindex]
+        self.data = da.reshape(i/nbl,nbl,nchan)
+        self.flags = fl.reshape(i/nbl,nbl,nchan)
+        self.time = ti[::nbl]
+        print
+        print 'Data read!'
+        print 'Shape of Data, Flags, Time:'
+        print self.data.shape, self.flags.shape, self.time.shape
+        print 
 
 
-def vis(da,chans=chans):
+    def prep(self):
+        self.data = (self.data * self.flags)[:,:][:,:,self.chans] 
+        print 'Data flagged and trimmed in channels.'
+
+
+    def spec(self):
+        noautos = self.noautos
+        autos = self.autos
+        chans = self.chans
+        time = self.time
+        data = self.data
+
 #reorganize into dimensions of integration, baseline, channel
-    print 'Original data type and shape: ', type(da), ', ', da.shape
+        print 'Plotting spectrgram.'
+        abs = n.abs(data[:,noautos][:,:,chans].mean(axis=1))
 
-    abs = n.abs(da[:,noautos][:,:,chans].mean(axis=1))
-
-    p.figure(1)
-    day0 = int(time[0])
-    ax = p.imshow(abs, aspect='auto')
+        p.figure(1)
+        day0 = int(time[0])
+        ax = p.imshow(abs, aspect='auto')
 #    ax = p.imshow(abs, aspect='auto', extent=(0,max(chans),max(time-day0),min(time-day0)))
-
 #    p.axis([-0.5,len(chans)+0.5,100,0])
-    p.colorbar(ax)
+        p.colorbar(ax)
 
-#def dedisperse(da):
-    
 
-# default stuff
-time,data,flags = load()
-data2 = data*flags
-vis(data2)
+    def dmmask(self, dm = 60., t0 = 0., show=0):
+        """Takes dispersion measure in pc/cm3 and time offset from first integration in seconds.
+        """
+
+        sfreq = self.sfreq
+        sdf = self.sdf
+        nchan = self.nchan
+        chans = self.chans
+
+        # initialize mask (false=0)
+        mask = n.zeros((self.data.shape[0],self.data.shape[2]),dtype=bool)   # could get clever here.  use integer code to stack dm masks in unique way
+        time = 24*3600*(self.time - self.time[0])     # relative time array in seconds
+        freq = sfreq + chans * sdf             # freq array in GHz
+
+        # given freq, dm, dfreq, calculate pulse time and duration
+        pulset = 4.2e-3 * dm * freq**(-2) + t0  # time in seconds
+        pulsedt = 8.3e-6 * dm * (1000*sdf) * freq**(-3)   # dtime in seconds
+
+        for ch in range(mask.shape[1]):
+            ontime = n.where(((pulset[ch] + pulsedt[ch]/2.) >= time) & ((pulset[ch] - pulsedt[ch]/2.) <= time))
+            print ontime
+            mask[ontime, ch] = True
+
+        if show:
+            ax = p.imshow(mask, aspect='auto')
+            p.colorbar(ax)
+
+        return mask
+
+    def dedisperse(self,da):
+        pass
+
+
+if __name__ == '__main__':
+    # default stuff
+    print 'Greetings, human.'
+    print ''
+
+    pv = poco()
+    pv.load('poco_crab_candpulse.mir')
+    pv.flag()
+    pv.vis()
