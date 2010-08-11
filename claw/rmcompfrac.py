3332d6906e2cf6b23fdff13e34f7768fa5474cd6
claw <claw@9b194dde-2b4d-0410-98f0-bba1668eb47e>
2010-08-11 22:30:03 +0000

    rm peak error histogram plot done right.
    git-svn-id: https://svn.hcro.org/mmm@888 9b194dde-2b4d-0410-98f0-bba1668eb47e

--- claw/rmcompfrac.py
+++ claw/rmcompfrac.py
@@ -17,11 +17,20 @@ rm1 = numpy.array(f.columns[4])
 erm1 = numpy.array(f.columns[6])
 rm2 = numpy.array(f.columns[12])
 erm2 = numpy.array(f.columns[13])
-
-print 'Set arrays of lengths %d, %d, %d' % (len(n), len(4m), len(rm2))
+print 'Set arrays of lengths %d, %d, %d' % (len(n), len(rm1), len(rm2))
 
 sigdiff = (rm1 - rm2)/numpy.sqrt(erm1**2 + erm2**2)
 
+hist, bin_edges = numpy.histogram(sigdiff,range=(-17,17),bins=34, normed=True)
+
+gaussian = lambda amp,x,sigma: amp/(numpy.sqrt(2 * numpy.pi)*sigma) * numpy.exp(-0.5*(x/numpy.array([sigma]))**2)  # normalized gaussian SNR distribution for comparison.
+bins = [bin_edges[i:i+2].mean() for i in range(len(bin_edges)-1)]
+pylab.plot(bins, gaussian(42,bins,1), label='Gaussian')
+pylab.plot(bins, 42*hist, label='Observed')
+pylab.xlabel('RM-highres - RM-lowres (sigma)')
+pylab.ylabel('Number of Sources')
+pylab.legend()
+pylab.show()
 
 #oldn = ''
 #for name in n:
