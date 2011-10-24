dfbbf1f1cd4f8a0191c28a7cf4793dbc3c21433f
claw <claw@9b194dde-2b4d-0410-98f0-bba1668eb47e>
2011-10-24 23:08:43 +0000

    fixed bug in inc det. added bispectrum
    git-svn-id: https://svn.hcro.org/mmm@1307 9b194dde-2b4d-0410-98f0-bba1668eb47e

--- claw/det_noise.py
+++ claw/det_noise.py
@@ -10,13 +10,18 @@ import random
 class noise:
     def __init__(self, len=100, std=1.):
         self.std = std
+        self.len = len
 
         self.data = n.zeros(len, dtype='complex')
+        self.datat = n.zeros(len+2, dtype='complex')
         for i in range(len):
             self.data[i] = n.complex(random.gauss(0, std), random.gauss(0, std))
+        for i in range(len+2):
+            self.datat[i] = n.complex(random.gauss(0, std), random.gauss(0, std))
 
     def add_source(self, flux=1+0j):
         self.data = self.data + flux
+        self.datat = self.datat + flux
 
     def det(self,show=0):
         self.datadet = n.abs(self.data)
@@ -25,39 +30,61 @@ class noise:
             print 'datadet:', self.datadet
         return self.datadet
 
+    def trip(self):
+        bisp = []
+        for i in range(self.len):
+            bisp.append(self.datat[i] * self.datat[i+1] * self.datat[i+2])
+        self.bisp = n.array(bisp)
+
     def show(self):
-        p.plot(self.data.real, self.data.imag,'.')
+        p.plot(self.bisp, '.')
         p.show()
 
-def repeat(num=351, source=0+0j, show=0):
+def repeat(num=100, source=0+0j, show=0):
     bglen = 1000
 
     # make detected visibilities on source
     nn = noise(len=num)
     nn.add_source(source)
+    nn.trip()
+    bion = nn.bisp.real.mean()
     dmon = nn.det().mean()
 
     # make background detected visibilities
     dmarr = []
+    mdarr = []
+    biarr = []
     for i in range(bglen):
         nnoff = noise(len=num)
+        nnoff.trip()
+        mdarr.append(n.abs(nnoff.data.mean()))
+        biarr.append(nnoff.bisp.real.mean())
         dmarr.append(nnoff.det().mean())
+    mdarr = n.array(mdarr)
     dmarr = n.array(dmarr)
+    biarr = n.array(biarr)
+
     meanoff = dmarr.mean()
     stdoff = dmarr.std()
     dmsig = (dmon - meanoff)/stdoff
 
+    meanoff = biarr.mean()
+    stdoff = biarr.std()
+    bisig = (bion - meanoff)/stdoff
+
     # make background mean visibilities and det significance
-    nnoff = noise(len=bglen*num)
-    meanoff = n.abs(nnoff.data.mean())
-    stdoff = nnoff.data.std()/n.sqrt(num)
+    meanoff = mdarr.mean()
+    stdoff = mdarr.std()
+
     nn = noise(len=num)
     nn.add_source(source)
-    mdsig = (n.abs(nn.data.mean()) - meanoff)/stdoff
+    dmon = n.abs(nn.data.mean())
+    mdsig = (dmon - meanoff)/stdoff
 
     intsig = n.abs(source)/nn.std
     print 'snr per int:', intsig
     print 'det then mean:', dmsig, 'sigma'
     print 'mean then det:', mdsig, 'sigma'
+    print 'bispectrum:', bisig, 'sigma'
 
-    return n.array([intsig, dmsig, mdsig])
+    return n.array([intsig, dmsig, mdsig, bisig])
