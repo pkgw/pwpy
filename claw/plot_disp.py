55b4c4c8c7e9e71b6beb3e38cb05a6449a02ca41
claw <claw@9b194dde-2b4d-0410-98f0-bba1668eb47e>
2011-11-18 03:28:45 +0000

    plotting changes
    git-svn-id: https://svn.hcro.org/mmm@1323 9b194dde-2b4d-0410-98f0-bba1668eb47e

--- claw/plot_disp.py
+++ claw/plot_disp.py
@@ -13,10 +13,10 @@ def drift(dm,f,bw=0.128):
 def scat(dm,f): 
     return 0.01 * (f/3.)**(-4) * (dm/1000.)**2     # needs to be refined
 
-bw = 0.128   # bandwidth in GHz
-tint = 0.01   # integration time in seconds
-dmarr = n.arange(10,2000,10)   # dm grid in pc/cm3
-farr = n.arange(10,100)/10.    # frequency grid in GHz
+bw = 0.1   # bandwidth in GHz
+tint = 1.   # integration time in seconds
+dmarr = n.arange(10,1000,10)   # dm grid in pc/cm3
+farr = n.arange(10,30)/100.    # frequency grid in GHz
 
 # initialize arrays
 driftarr = [drift(dmarr, farr[0], bw=bw)]
@@ -34,10 +34,11 @@ mask = n.where(driftarr > tint, raarr, zz)   # area where drift is larger than i
 # plot
 p.imshow(n.log10(mask), aspect='auto', interpolation='nearest', origin='lower', extent= (dmarr.min(), dmarr.max(), farr.min(), farr.max()))
 
-p.title('drift time over scattering time')
-p.xlabel('DM')
-p.ylabel('freq')
+p.title('Log of Drift time over scattering time, %d integrations' % (tint))
+p.xlabel('DM (pc cm$^{-3}$)')
+p.ylabel('Frequency (GHz)')
 p.colorbar()
+p.show()
 
 # can add plot of SNR of pulse peak, assuming spectral index
 # can add plot of drift SNR
