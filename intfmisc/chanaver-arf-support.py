4afdf621baa78c7c211e3497ace16d7fb95eaa8b
Peter Williams <peter@newton.cx>
2012-02-27 14:08:06 -0800

    Prep for mooching off of the miriad-python examples

---
+++ intfmisc/chanaver-arf-support.py
@@ -0,0 +1,23 @@
+
+try:
+    from awff import MultiprocessMake
+    from arf.visutil import propagateHeaders
+except ImportError:
+    pass
+else:
+    __all__ += ['asMake']
+
+    def _asmake (context, vis=None, params=None):
+        params = dict (params) # copy so we don't modify:
+        naver = params.pop ('naver', 0)
+
+        context.ensureParent ()
+        out = VisData (context.fullpath ())
+        out.delete ()
+
+        channelAverageWithSetup (vis, out, naver, **params)
+        propagateHeaders (vis, out)
+        return out
+
+    asMake = MultiprocessMake ('vis params', 'out', _asmake,
+                               [None, {}])
