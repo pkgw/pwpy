2a394708a439203c09c3edda915c310202841aec
Peter Williams <peter@newton.cx>
2012-03-29 10:29:33 -0700

    quickutils/utils/popoption.py: strip docstring by default, compactify

--- quickutils/utils/popoption.py
+++ quickutils/utils/popoption.py
@@ -1,27 +1,22 @@
 ## Copyright 2012 Peter Williams
 ## This work is dedicated to the public domain.
-
-def popoption (ident, args=None):
-    """A lame routine for grabbing command-line arguments. Returns a
-    boolean indicating whether the option was present. If it was, it's
-    removed from the argument string. Because of the lame behavior,
-    options can't be combined, and non-boolean options aren't
-    supported. Operates on sys.argv by default.
-
-    Note that this will proceed merrily if argv[0] matches your option.
-    """
-
-    if args is None:
-        args = sys.argv
-
+def popoption (ident, argv=None):
+##<
+## A lame routine for grabbing command-line arguments. Returns a
+## boolean indicating whether the option was present. If it was, it's
+## removed from the argument string. Because of the lame behavior,
+## options can't be combined, and non-boolean options aren't
+## supported. Operates on sys.argv by default.
+##
+## Note that this will proceed merrily if argv[0] matches your option.
+##>
+    if argv is None:
+        from sys import argv
     if len (ident) == 1:
         ident = '-' + ident
     else:
         ident = '--' + ident
-
-    found = ident in args
-
+    found = ident in argv
     if found:
-        args.remove (ident)
-
+        argv.remove (ident)
     return found
