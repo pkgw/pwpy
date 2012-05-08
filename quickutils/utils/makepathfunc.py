c556cc72896b28af29bf66aeea375ce4ad273b91
Peter Williams <peter@newton.cx>
2012-05-08 12:30:11 -0700

    quickutils/utils/makepathfunc.py: join the basepath too

--- quickutils/utils/makepathfunc.py
+++ quickutils/utils/makepathfunc.py
@@ -1,11 +1,12 @@
 ## Copyright 2012 Peter Williams
 ## This work is dedicated to the public domain.
 ##
-def makepathfunc (base):
+def makepathfunc (*baseparts):
 ##<
 ## Return a function that joins paths onto some base directory.
 ##>
     from os.path import join
+    base = join (*baseparts)
     def pathfunc (*args):
         return join (base, *args)
     return pathfunc
