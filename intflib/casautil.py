fda4c9e71d884b139b08f28276baeefd8a5c4abd
Peter Williams <peter@newton.cx>
2012-12-05 17:26:07 -0500

    intflib/casautil.py: beginnings of casac utility library

---
+++ intflib/casautil.py
@@ -0,0 +1,67 @@
+"""casautil - utilities for using the CASA Python libraries
+"""
+
+__all__ = ('INVERSE_C_MS INVERSE_C_MNS pol_names pol_to_miriad tools').split ()
+
+
+# Some constants that can be useful
+
+INVERSE_C_MS  = 3.3356409519815204e-09 # inverse speed of light in m/s
+INVERSE_C_MNS = 3.3356409519815204 # inverse speed of light in m/ns
+
+pol_names = {
+    0: '?',
+    1: 'I', 2: 'Q', 3: 'U', 4: 'V',
+    5: 'RR', 6: 'RL', 7: 'LR', 8: 'LL',
+    9: 'XX', 10: 'XY', 11: 'YX', 12: 'YY',
+    13: 'RX', 14: 'RY', 15: 'LX', 16: 'LY',
+    17: 'XR', 18: 'XL', 19: 'YR', 20: 'YL',
+    21: 'PP', 22: 'PQ', 23: 'QP', 24: 'QQ',
+    25: 'RCirc', 26: 'Lcirc', 27: 'Lin', 28: 'Ptot', 29: 'Plin',
+    30: 'PFtot', 31: 'PFlin', 32: 'Pang',
+}
+
+pol_to_miriad = {
+    1: 1, 2: 2, 3: 3, 4: 4, # IQUV
+    5: -1, 6: -3, 7: -4, 8: -2, # R/L
+    9: -5, 10: -7, 11: -8, 12: -6, # X/Y
+    # rest are inexpressible
+}
+
+
+# Tool factories.
+
+class _Tools (object):
+    """This class is structured so that it supports useful tab-completion
+    interactively, but also so that new tools can be constructed if the
+    underlying library provides them."""
+
+    _builtinNames = ('agentflagger atmosphere calanalysis calibrater calplot componentlist '
+                     'coordsys deconvolver fitter flagger functional image imagepol '
+                     'imager logsink measures msmetadata ms msplot plotms regionmanager '
+                     'simulator spectralline quanta table tableplot utils vlafiller '
+                     'vpmanager').split ()
+
+    def __getattribute__ (self, n):
+        """Returns factories, not instances."""
+        # We need to make this __getattribute__, not __getattr__, only because
+        # we set the builtin names in the class __dict__ to enable tab-completion.
+        import casac
+
+        if hasattr (casac, 'casac'): # casapy >= 4.0?
+            t = getattr (casac.casac, n, None)
+            if t is None:
+                raise AttributeError ('tool "%s" not present' % n)
+            return t
+        else:
+            try:
+                return casac.homefinder.find_home_by_name (n + 'Home').create
+            except Exception:
+                # raised exception is class 'homefinder.error'; it appears unavailable
+                # on the Python layer
+                raise AttributeError ('tool "%s" not present' % n)
+
+for n in _Tools._builtinNames:
+    setattr (_Tools, n, None) # ease autocompletion
+
+tools = _Tools ()
