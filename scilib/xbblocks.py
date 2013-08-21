f4bcad25fefdfa174ad46878cf5ac1b2f0773911
Peter Williams <peter@newton.cx>
2013-08-21 14:16:30 -0400

    scilib/xbblocks.py: support multiple good-time intervals (GTIs)
    As expected this required some thinking, but it wasn't too hard to implement
    overall. We key off of the insight in Scargle+13, which is that gaps between
    GTIs can just be ignored in the fundamental analysis.
    
    xbin/chandrabblock was updated to match. It also got a few very small doc and
    polish fixes.

--- scilib/xbblocks.py
+++ scilib/xbblocks.py
@@ -172,16 +172,33 @@ def binbblock (widths, counts, p0=0.05):
     return info
 
 
-def ttbblock (tstart, tstop, times, p0=0.05):
+def ttbblock (tstarts, tstops, times, p0=0.05):
+    tstarts = np.asarray (tstarts)
+    tstops = np.asarray (tstops)
     times = np.asarray (times)
-    dt = times[1:] - times[:-1]
 
-    if np.any (dt < 0):
+    if tstarts.size != tstops.size:
+        raise ValueError ('must have same number of starts and stops')
+
+    ngti = tstarts.size
+
+    if ngti < 1:
+        raise ValueError ('must have at least one goodtime interval')
+    if np.any ((tstarts[1:] - tstarts[:-1]) <= 0):
+        raise ValueError ('tstarts must be ordered and distinct')
+    if np.any ((tstops[1:] - tstops[:-1]) <= 0):
+        raise ValueError ('tstarts must be ordered and distinct')
+    if np.any (tstarts >= tstops):
+        raise ValueError ('tstarts must come before tstops')
+    if np.any ((times[1:] - times[:-1]) < 0):
         raise ValueError ('times must be ordered')
-    if times.min () < tstart:
-        raise ValueError ('no times may be smaller than tstart')
-    if times.max () > tstop:
-        raise ValueError ('no times may be larger than tstop')
+    if times.min () < tstarts[0]:
+        raise ValueError ('no times may be smaller than first tstart')
+    if times.max () > tstops[-1]:
+        raise ValueError ('no times may be larger than last tstop')
+    for i in xrange (1, ngti):
+        if np.where ((times > tstops[i-1]) & (times < tstarts[i]))[0].size:
+            raise ValueError ('no times may fall in goodtime gap #%d' % i)
     if p0 < 0 or p0 >= 1.:
         raise ValueError ('p0 must lie within [0, 1)')
 
@@ -191,28 +208,67 @@ def ttbblock (tstart, tstop, times, p0=0.05):
     counts = np.empty (nunique)
     counts[:-1] = uidxs[1:] - uidxs[:-1]
     counts[-1] = times.size - uidxs[-1]
-    #assert counts.sum () == times.size
+    assert counts.sum () == times.size
 
-    midpoints = 0.5 * (utimes[1:] + utimes[:-1]) # size: nunique - 1
-    edges = np.concatenate (([tstart], midpoints, [tstop])) # size: nunique + 1
-    widths = edges[1:] - edges[:-1] # size: nunique
-    #assert widths.sum () == tstop - tstart
+    # we grow these arrays with concats, which will perform badly with lots of
+    # GTIs. Not expected to be a big deal.
+    widths = np.empty (0)
+    ledges = np.empty (0)
+    redges = np.empty (0)
 
+    for i in xrange (ngti):
+        tstart, tstop = tstarts[i], tstops[i]
+
+        w = np.where ((utimes >= tstart) & (utimes <= tstop))[0]
+
+        if not w.size:
+            # No events during this goodtime! We have to insert a zero-count
+            # event block. This may break assumptions within binbblock()?
+
+            # j = idx of first event after this GTI
+            wafter = np.where (utimes > tstop)[0]
+            if wafter.size:
+                j = wafter[0]
+            else:
+                j = utimes.size
+            # NOTE: this assert breaks if we're here and i == 0
+            assert np.where (utimes < tstart)[0][-1] == j - 1
+
+            counts = np.concatenate ((counts[:j], [0], counts[j:]))
+            widths = np.concatenate ((widths, [tstop - tstart]))
+            ledges = np.concatenate ((ledges, [tstart]))
+            redges = np.concatenate ((redges, [tstop]))
+        else:
+            gtutimes = utimes[w]
+            midpoints = 0.5 * (gtutimes[1:] + gtutimes[:-1]) # size: n - 1
+            gtedges = np.concatenate (([tstart], midpoints, [tstop])) # size: n + 1
+            gtwidths = gtedges[1:] - gtedges[:-1] # size: n
+            assert gtwidths.sum () == tstop - tstart
+            widths = np.concatenate ((widths, gtwidths))
+            ledges = np.concatenate ((ledges, gtedges[:-1]))
+            redges = np.concatenate ((redges, gtedges[1:]))
+
+    assert counts.size == widths.size
     info = binbblock (widths, counts, p0=p0)
-    info.ledges = edges[info.blockstarts]
-    info.redges = np.concatenate ((edges[info.blockstarts[1:]], [tstop]))
+    info.ledges = ledges[info.blockstarts]
+    # The right edge of the i'th block is the right edge of its rightmost
+    # bin, which is the bin before the leftmost bin of the (i+1)'th block:
+    info.redges = np.concatenate ((redges[info.blockstarts[1:] - 1], [redges[-1]]))
     info.midpoints = 0.5 * (info.ledges + info.redges)
     return info
 
 
-def bsttbblock (times, tstart, tstop, p0=0.05, nbootstrap=512):
+def bsttbblock (times, tstarts, tstops, p0=0.05, nbootstrap=512):
     np.seterr ('raise')
     times = np.asarray (times)
+    tstarts = np.asarray (tstarts)
+    tstops = np.asarray (tstops)
+
     nevents = times.size
     if nevents < 1:
         raise ValueError ('must be given at least 1 event')
 
-    info = ttbblock (tstart, tstop, times, p0)
+    info = ttbblock (tstarts, tstops, times, p0)
 
     # Now bootstrap resample to assess uncertainties on the bin heights. This
     # is the approach recommended by Scargle+.
@@ -223,7 +279,7 @@ def bsttbblock (times, tstart, tstop, p0=0.05, nbootstrap=512):
     for _ in xrange (nbootstrap):
         bstimes = times[np.random.randint (0, times.size, times.size)]
         bstimes.sort ()
-        bsinfo = ttbblock (tstart, tstop, bstimes, p0)
+        bsinfo = ttbblock (tstarts, tstops, bstimes, p0)
         blocknums = np.minimum (np.searchsorted (bsinfo.redges, info.midpoints),
                                 bsinfo.nblocks - 1)
         samprates = bsinfo.rates[blocknums]
