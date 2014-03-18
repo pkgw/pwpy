bc9baa45f2ccc3dd4761afa88214d0390e92dcda
Peter Williams <peter@newton.cx>
2014-03-17 23:41:16 -0400

    scilib/lsqmdl.py: untested multi-component modeling framework
    I'm not happy to be committing this without a use case, but I put a bunch of
    work into it and I don't want to lose it. I'll fix it or delete it if it
    doesn't work in practice.

--- scilib/lsqmdl.py
+++ scilib/lsqmdl.py
@@ -200,3 +200,356 @@ class ScaleModel (_ModelBase):
         self.resids = self.y - self.modely
         self.rchisq = ((self.resids * self.invsigma)**2).sum () / (self.x.size - 1)
         return self
+
+
+# lmmin-based model-fitting when the model is broken down into composable
+# components.
+
+class ModelComponent (object):
+    npar = 0
+    name = None
+    paramnames = ()
+
+    setguess = None
+    setvalue = None
+    setlimit = None
+
+    def __init__ (self, name=None):
+        self.name = name
+
+    def _param_names (self):
+        """Overridable in case the list of parameter names needs to be
+        generated on the fly."""
+        return self.paramnames
+
+    def finalize_setup (self):
+        """If the component has subcomponents, this should set their `name`,
+        `setguess`, `setvalue`, and `setlimit` properties. It should also
+        set `npar` (on self) to the final value."""
+        pass
+
+    def prep_params (self):
+        """This should make any necessary calls to `setvalue` or `setlimit`,
+        though in straightforward cases it should just be up to the user to
+        do this. If the component has subcomponents, their `prep_params`
+        functions should be called."""
+        pass
+
+    def model (self, pars, x, y):
+        """Modify `y` based on `x` and `pars`."""
+        pass
+
+    def deriv (self, pars, x, jac):
+        """Compute the Jacobian. `jac[i]` is d`y`/d`pars[i]`."""
+        pass
+
+    def extract (self, pars, perr, cov):
+        """Extract fit results into the object for ease of inspection."""
+        self.covar = cov
+
+
+class ComposedModel (_ModelBase):
+    def __init__ (self, component, x, y, invsigma=None):
+        if component is not None:
+            self.setcomponent (component)
+        if x is not None:
+            self.setdata (x, y, invsigma)
+
+
+    def _component_setguess (self, vals, ofs=0):
+        vals = np.asarray (vals)
+        if ofs < 0 or ofs + vals.size > self.component.npar:
+            raise ValueError ('ofs %d, vals.size %d, npar %d' %
+                              (ofs, vals.size, self.component.npar))
+        self.force_guess[ofs:ofs+vals.size] = vals
+
+
+    def _component_setvalue (self, cidx, val, fixed=False):
+        if cidx < 0 or cidx >= self.component.npar:
+            raise ValueError ('cidx %d, npar %d' % (cidx, self.component.npar))
+        self.lm_prob.pValue (cidx, val, fixed=fixed)
+        self.force_guess[cidx] = val
+
+
+    def _component_setlimit (self, cidx, lower=-np.inf, upper=np.inf):
+        if cidx < 0 or cidx >= self.component.npar:
+            raise ValueError ('cidx %d, npar %d' % (cidx, self.component.npar))
+        self.lm_prob.pLimit (cidx, lower, upper)
+
+
+    def setcomponent (self, component):
+        self.component = component
+
+        component.setguess = self._component_setguess
+        component.setvalue = self._component_setvalue
+        component.setlimit = self._component_setlimit
+        component.finalize_setup ()
+
+        import lmmin
+        self.lm_prob = lmmin.Problem (component.npar)
+        self.force_guess = np.empty (component.npar)
+        self.force_guess.fill (np.nan)
+        self.paramnames = list (component._param_names ())
+
+        component.prep_params ()
+
+
+    def solve (self, guess=None):
+        if guess is None:
+            guess = self.force_guess
+        else:
+            guess = np.array (guess, dtype=np.float, ndmin=1, copy=True)
+
+            for i in xrange (self.force_guess.size):
+                if np.isfinite (self.force_guess[i]):
+                    guess[i] = self.force_guess[i]
+
+        x = self.x
+
+        def model (pars, outputs):
+            outputs.fill (0)
+            self.component.model (pars, x, outputs)
+
+        def deriv (pars, jac):
+            self.component.deriv (pars, x, jac)
+
+        self.lm_prob.setResidualFunc (self.y, self.invsigma, model, deriv)
+        self.lm_soln = soln = self.lm_prob.solve (guess)
+
+        self.params = soln.params
+        self.perror = soln.perror
+        self.covar = soln.covar
+
+        def modelfunc (x):
+            y = np.zeros_like (x)
+            self.component.model (self.params, x, y)
+            return y
+
+        self.modelfunc = modelfunc
+        self.modely = modelfunc (x)
+        if soln.ndof > 0:
+            self.rchisq = soln.fnorm / soln.ndof
+        self.resids = self.y - self.modely
+
+        self.component.extract (soln.params, soln.perror, soln.covar)
+        return self
+
+
+# Now specific components useful in the above framework. The general strategy
+# is to err on the side of having additional parameters in the individual
+# classes, and the user can call setvalue() to fix them if they're not needed.
+
+class AddConstantComponent (ModelComponent):
+    npar = 1
+    paramnames = ('value', )
+
+    def model (self, pars, x, y):
+        y += pars[0]
+
+    def deriv (self, pars, x, jac):
+        jac[0] = 1.
+
+    def extract (self, pars, perr, cov):
+        self.covar = cov
+        self.f_value = pars[0]
+        self.u_value = perr[0]
+
+
+class AddSinComponent (ModelComponent):
+    npar = 3
+    paramnames = ('amp', 'angfreq', 'phase')
+
+    def model (self, pars, x, y):
+        y += pars[0] * np.sin (pars[1] * x + pars[2])
+
+    def deriv (self, pars, x, jac):
+        th = pars[1] * x + pars[2]
+        jac[0] = np.sin (th)
+        jac[2] = pars[0] * np.cos (th)
+        jac[1] = jac[1] * x
+
+    def extract (self, pars, perr, cov):
+        self.covar = cov
+        self.f_amp, self.f_angfreq, self.f_phase = pars
+        self.u_amp, self.u_angfreq, self.u_phase = perr
+
+        if self.f_amp < 0:
+            self.f_amp *= -1
+            self.f_phase += np.pi
+
+        self.f_phase = (self.f_phase + np.pi) % (2 * np.pi) - np.pi
+
+
+class SeriesComponent (ModelComponent):
+    """Apply a set of subcomponents in series, isolating each from the other. This
+    is only valid if every subcomponent except the first is additive --
+    otherwise, the Jacobian won't be right."""
+
+    def __init__ (self, name=None, components=()):
+        super (SeriesComponent, self).__init__ (name)
+        self.components = list (components)
+
+
+    def add (self, component):
+        """This helps, but direct manipulation of self.components should be
+        supported."""
+        self.components.append (component)
+        return self
+
+
+    def _param_names (self):
+        for c in self.components:
+            pfx = c.name + '.' if c.name is not None else ''
+            for p in c._param_names ():
+                yield pfx + p
+
+
+    def _offset_setguess (self, ofs, npar, vals, subofs=0):
+        vals = np.asarray (vals)
+        if subofs < 0 or subofs + vals.size > npar:
+            raise ValueError ('subofs %d, vals.size %d, npar %d' %
+                              (subofs, vals.size, npar))
+        return self.setguess (vals, ofs + subofs)
+
+
+    def _offset_setvalue (self, ofs, npar, cidx, value, fixed=False):
+        if cidx < 0 or cidx >= npar:
+            raise ValueError ('cidx %d, npar %d' % (cidx, npar))
+        return self.setvalue (ofs + cidx, value, fixed)
+
+
+    def _offset_setlimit (self, ofs, npar, cidx, lower=-np.inf, upper=np.inf):
+        if cidx < 0 or cidx >= npar:
+            raise ValueError ('cidx %d, npar %d' % (cidx, npar))
+        return self.setlimit (ofs + cidx, lower, upper)
+
+
+    def finalize_setup (self):
+        from functools import partial
+
+        ofs = 0
+
+        for i, c in enumerate (self.components):
+            if c.name is None:
+                c.name = 'c%d' % i
+
+            c.setguess = partial (self._offset_setguess, ofs, c.npar)
+            c.setvalue = partial (self._offset_setvalue, ofs, c.npar)
+            c.setlimit = partial (self._offset_setlimit, ofs, c.npar)
+            ofs += c.npar
+
+        self.npar = ofs
+
+
+    def prep_params (self):
+        for c in self.components:
+            c.prep_params ()
+
+
+    def model (self, pars, x, y):
+        ofs = 0
+
+        for c in self.components:
+            p = pars[ofs:ofs+c.npar]
+            c.model (p, x, y)
+            ofs += c.npar
+
+
+    def deriv (self, pars, x, jac):
+        ofs = 0
+
+        for c in self.components:
+            p = pars[ofs:ofs+c.npar]
+            j = jac[ofs:ofs+c.npar]
+            c.deriv (p, x, j)
+            ofs += c.npar
+
+
+    def extract (self, pars, perr, cov):
+        ofs = 0
+
+        for c in self.components:
+            n = c.npar
+
+            spar = pars[ofs:ofs+n]
+            serr = perr[ofs:ofs+n]
+            scov = cov[ofs:ofs+n,ofs:ofs+n]
+            c.extract (spar, serr, scov)
+            ofs += n
+
+
+class ScaleComponent (ModelComponent):
+    npar = 1
+
+    def __init__ (self, name=None, subcomp=None):
+        super (ScaleComponent, self).__init__ (name)
+        self.setsubcomp (subcomp)
+
+
+    def setsubcomp (self, subcomp):
+        self.subcomp = subcomp
+        return self
+
+
+    def _param_names (self):
+        yield 'factor'
+
+        pfx = self.subcomp.name + '.' if self.subcomp.name is not None else ''
+        for p in self.subcomp._param_names ():
+            yield pfx + p
+
+
+    def _sub_setguess (self, npar, cidx, vals, ofs=0):
+        vals = np.asarray (vals)
+        if ofs < 0 or ofs + vals.size > npar:
+            raise ValueError ('ofs %d, vals.size %d, npar %d' %
+                              (ofs, vals.size, npar))
+        return self.setguess (vals, ofs + 1)
+
+
+    def _sub_setvalue (self, npar, cidx, value, fixed=False):
+        if cidx < 0 or cidx >= npar:
+            raise ValueError ('cidx %d, npar %d' % (cidx, npar))
+        return self.setvalue (1 + cidx, value, fixed)
+
+
+    def _sub_setlimit (self, npar, cidx, lower=-np.inf, upper=np.inf):
+        if cidx < 0 or cidx >= npar:
+            raise ValueError ('cidx %d, npar %d' % (cidx, npar))
+        return self.setlimit (1 + cidx, lower, upper)
+
+
+    def finalize_setup (self):
+        if self.subcomp.name is None:
+            self.subcomp.name = 'c'
+
+        from functools import partial
+        self.subcomp.setvalue = partial (self._sub_setvalue, self.subcomp.npar)
+        self.subcomp.setlimit = partial (self._sub_setvalue, self.subcomp.npar)
+        self.subcomp.finalize_setup ()
+
+        self.npar = self.subcomp.npar + 1
+
+
+    def prep_params (self):
+        self.subcomp.prep_params ()
+
+
+    def model (self, pars, x, y):
+        self.subcomp.model (pars[1:], x, y)
+        y *= pars[0]
+
+
+    def deriv (self, pars, x, jac):
+        self.subcomp.model (pars[1:], x, jac[0])
+
+        self.subcomp.deriv (pars[1:], x, jac[1:])
+        jac[1:] *= pars[0]
+
+
+    def extract (self, pars, perr, cov):
+        self.f_factor = pars[0]
+        self.u_factor = perr[0]
+        self.c_factor = cov[0]
+
+        self.subcomp.extract (pars[1:], perr[1:], cov[1:,1:])
